16fb41e6094311e423b42a34145337dbce7ccda8
Shariq Ansari <sharique.rik@gmail.com>
2023-07-25 11:37:48 +0530

    fix: created crm lead doctype and loaded the data in listview
    also created some related doctypes

--- frontend/src/pages/Leads.vue
+++ frontend/src/pages/Leads.vue
@@ -4,51 +4,75 @@
 
 <script setup>
 import ListView from '../components/ListView.vue'
+import { computed } from 'vue'
+import { createListResource } from 'frappe-ui'
 
 const title = 'Leads'
 
+const leads = createListResource({
+  type: 'list',
+  doctype: 'CRM Lead',
+  fields: [
+    'name',
+    'first_name',
+    'last_name',
+    'organization_name',
+    'status',
+    'email',
+    'mobile_no',
+    'lead_owner',
+    'modified',
+  ],
+  orderBy: 'modified asc',
+  cache: 'Leads',
+  pageLength: 999,
+  auto: true,
+})
+leads.fetch()
+
 const columns = [
   {
-    label: 'Title',
-    key: 'title',
-    size: 'flex-1',
-  },
-  {
-    label: 'Company',
-    key: 'company',
+    label: 'Full name',
+    key: 'full_name',
     size: 'w-44',
   },
   {
-    label: 'Stage',
-    key: 'stage',
-    size: 'w-40',
-  },
-  {
-    label: 'Contact',
-    key: 'contact',
+    label: 'Organization',
+    key: 'organization_name',
     size: 'w-44',
   },
   {
-    label: 'Closed Date',
-    key: 'closedDate',
+    label: 'Status',
+    key: 'status',
     size: 'w-44',
   },
   {
-    label: 'Value',
-    key: 'value',
-    size: 'w-40',
-    align: 'text-right',
+    label: 'Email',
+    key: 'email',
+    size: 'w-44',
+  },
+  {
+    label: 'Mobile no',
+    key: 'mobile_no',
+    size: 'w-44',
+  },
+  {
+    label: 'Lead owner',
+    key: 'lead_owner',
+    size: 'w-44',
   },
 ]
 
-const rows = [
-  {
-    title: 'Summer Catalog',
-    company: 'Stripe',
-    stage: 'Qualified',
-    contact: 'Angela Bower',
-    closedDate: '27 Sept, 2023',
-    value: 99000.0,
-  },
-]
+const rows = computed(() => {
+  return leads.data?.map((lead) => {
+    return {
+      full_name: lead.first_name + ' ' + lead.last_name,
+      organization_name: lead.organization_name,
+      status: lead.status,
+      email: lead.email,
+      mobile_no: lead.mobile_no,
+      lead_owner: lead.lead_owner,
+    }
+  })
+})
 </script>
