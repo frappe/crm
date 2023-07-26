893e09290e9a4295b530dc1f6e766c8b4931e6a4
Shariq Ansari <sharique.rik@gmail.com>
2023-07-26 22:46:36 +0530

    build(deps): bump frappeui to latest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { getProxyOptions } from 'frappe-ui/src/utils/vite-dev-server'
+import frappeui from 'frappe-ui/vite'
 import { webserver_port } from '../../../sites/common_site_config.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 8080,
-    proxy: getProxyOptions({ port: webserver_port }),
-  },
+  plugins: [frappeui(), vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
