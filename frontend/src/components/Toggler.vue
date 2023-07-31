1b54e9c721c29cc5858f396ddac70067f339ccb2
Shariq Ansari <sharique.rik@gmail.com>
2023-07-31 12:16:07 +0530

    feat: added toggler component

---
+++ frontend/src/components/Toggler.vue
@@ -0,0 +1,24 @@
+<template>
+  <slot v-bind="{ opened, open, close, toggle }"></slot>
+</template>
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  isOpened: {
+    type: Boolean,
+    default: true,
+  },
+})
+function toggle() {
+  opened.value = !opened.value
+}
+
+function open() {
+  opened.value = true
+}
+
+function close() {
+  opened.value = false
+}
+let opened = ref(props.isOpened)
+</script>
