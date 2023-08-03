cd118cf08c31a1ecfbc6846940554ec811877262
Shariq Ansari <sharique.rik@gmail.com>
2023-08-03 21:15:57 +0530

    fix: added UserAvatar component

---
+++ frontend/src/components/UserAvatar.vue
@@ -0,0 +1,21 @@
+<template>
+  <Avatar
+    v-if="user"
+    :label="getUser(user).full_name"
+    :image="getUser(user).user_image"
+    v-bind="$attrs"
+  />
+</template>
+<script setup>
+import { Avatar } from 'frappe-ui'
+import { usersStore } from '@/stores/users'
+
+const props = defineProps({
+  user: {
+    type: String,
+    default: null,
+  },
+})
+
+const { getUser } = usersStore()
+</script>
