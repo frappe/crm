352bebcd698bf9268c472768a68dfcae39efd2b6
Shariq Ansari <sharique.rik@gmail.com>
2023-08-10 19:14:29 +0530

    fix: added socket io to listen for cached documents

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,9 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 
 import { FrappeUI, Button, setConfig, frappeRequest } from 'frappe-ui'
+import socket from './socket'
+import { getCachedListResource } from 'frappe-ui/src/resources/listResource'
+import { getCachedResource } from 'frappe-ui/src/resources/resources'
 
 // create a pinia instance
 let pinia = createPinia()
@@ -19,3 +22,13 @@ app.use(router)
 
 app.component('Button', Button)
 app.mount('#app')
+
+socket.on('refetch_resource', (data) => {
+  if (data.cache_key) {
+    let resource =
+      getCachedResource(data.cache_key) || getCachedListResource(data.cache_key)
+    if (resource) {
+      resource.reload()
+    }
+  }
+})
