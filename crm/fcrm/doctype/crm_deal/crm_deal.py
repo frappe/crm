5b10e806690d9d30d3a8cba541c498e688b2b2bd
Shariq Ansari <sharique.rik@gmail.com>
2023-11-07 16:02:28 +0530

    fix: added sort fields for deal

--- crm/fcrm/doctype/crm_deal/crm_deal.py
+++ crm/fcrm/doctype/crm_deal/crm_deal.py
@@ -6,4 +6,14 @@ from frappe.model.document import Document
 
 
 class CRMDeal(Document):
-	pass
+	@staticmethod
+	def sort_options():
+		return [
+			{ "label": 'Created', "value": 'creation' },
+			{ "label": 'Modified', "value": 'modified' },
+			{ "label": 'Status', "value": 'status' },
+			{ "label": 'Deal owner', "value": 'deal_owner' },
+			{ "label": 'Organization', "value": 'organization' },
+			{ "label": 'Email', "value": 'email' },
+			{ "label": 'Mobile no', "value": 'mobile_no' },
+		]
