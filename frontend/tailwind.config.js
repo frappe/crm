fe47bf8af1d4b63fbcfda20300f57e00cbe537f0
Shariq Ansari <sharique.rik@gmail.com>
2023-11-29 15:48:51 +0530

    fix: safelist some tailwind classes to avoid purging dynamically generated lead/deal status color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-  presets: [
-    require('frappe-ui/src/utils/tailwind.config')
-  ],
+  presets: [require('frappe-ui/src/utils/tailwind.config')],
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}",
-    "../node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}',
+    '../node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}',
   ],
+  safelist: [{ pattern: /!text-/ }],
   theme: {
     extend: {},
   },
