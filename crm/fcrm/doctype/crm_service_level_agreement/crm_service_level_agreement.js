8187d171544b8c9eec5fe241b294e13c34fa95fc
Shariq Ansari <sharique.rik@gmail.com>
2023-12-14 11:42:18 +0530

    fix: added holiday, follow up related fields on sla doctype

--- crm/fcrm/doctype/crm_service_level_agreement/crm_service_level_agreement.js
+++ crm/fcrm/doctype/crm_service_level_agreement/crm_service_level_agreement.js
@@ -1,8 +1,38 @@
 // Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("CRM Service Level Agreement", {
-// 	refresh(frm) {
+frappe.ui.form.on("CRM Service Level Agreement", {
+	// refresh(frm) {},
 
-// 	},
-// });
+	apply_sla_for_follow_up: function (frm) {
+		frm.trigger("toggle_follow_up_fields");
+	},
+
+	toggle_follow_up_fields: function (frm) {
+		if (cint(frm.doc.apply_sla_for_follow_up) === 1) {
+			frm.fields_dict.priorities.grid.update_docfield_property(
+				"follow_up_time",
+				"hidden",
+				0
+			);
+			frm.fields_dict.priorities.grid.update_docfield_property(
+				"follow_up_time",
+				"reqd",
+				1
+			);
+		} else {
+			frm.fields_dict.priorities.grid.update_docfield_property(
+				"follow_up_time",
+				"hidden",
+				1
+			);
+			frm.fields_dict.priorities.grid.update_docfield_property(
+				"follow_up_time",
+				"reqd",
+				0
+			);
+		}
+
+		frm.refresh_field("priorities");
+	},
+});
