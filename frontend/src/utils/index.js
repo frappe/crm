d30678d5de672172023184a9199502080416f5de
Shariq Ansari <sharique.rik@gmail.com>
2023-12-26 15:10:20 +0530

    fix: also round up seconds in SLA fulfilled duration calculation

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -15,7 +15,7 @@ export function formatTime(seconds) {
   const days = Math.floor(seconds / (3600 * 24))
   const hours = Math.floor((seconds % (3600 * 24)) / 3600)
   const minutes = Math.floor((seconds % 3600) / 60)
-  const remainingSeconds = seconds % 60
+  const remainingSeconds = Math.floor(seconds % 60)
 
   let formattedTime = ''
 
