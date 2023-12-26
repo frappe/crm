5454a4f8e55ea84d3160ed2514cae37699259580
Shariq Ansari <sharique.rik@gmail.com>
2023-12-26 16:15:48 +0530

    fix: only allow one default priority in priorities table

--- crm/fcrm/doctype/crm_service_level_agreement/crm_service_level_agreement.js
+++ crm/fcrm/doctype/crm_service_level_agreement/crm_service_level_agreement.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("CRM Service Level Agreement", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("CRM Service Level Agreement", {
+	validate(frm) {
+		let default_priority_count = 0;
+		frm.doc.priorities.forEach(function (row) {
+			if (row.default_priority) {
+				default_priority_count++;
+			}
+		});
+		if (default_priority_count > 1) {
+			frappe.throw(
+				__("There can only be one default priority in Priorities table")
+			);
+		}
+	},
+});
