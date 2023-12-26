5866adfa5e42d5bf0ca2d6aba138d8937c11dbdb
Shariq Ansari <sharique.rik@gmail.com>
2023-12-26 18:58:29 +0530

    fix: added validate email function in utils

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -106,3 +106,9 @@ export function formatNumberIntoCurrency(value) {
 export function startCase(str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+export function validateEmail(email) {
+  let regExp =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return regExp.test(email)
+}
