9a4a7658b9f440dca5d932cf657abb5e9df8cf17
Shariq Ansari <sharique.rik@gmail.com>
2023-12-27 16:30:51 +0530

    feat: added MultipleAvatar component

---
+++ frontend/src/components/MultipleAvatar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="avatars?.length" class="mr-1.5 flex flex-row-reverse items-center cursor-pointer">
+    <Tooltip :text="avatar.name" v-for="avatar in reverseAvatars" :key="avatar.name">
+      <Avatar
+        class="-mr-1.5 transform border-2 border-white transition hover:z-10 hover:scale-125"
+        shape="circle"
+        :image="avatar.image"
+        :label="avatar.label"
+        size="lg"
+      />
+    </Tooltip>
+  </div>
+</template>
+<script setup>
+import { Avatar, Tooltip } from 'frappe-ui'
+import { computed, defineModel } from 'vue'
+
+const avatars = defineModel()
+const reverseAvatars = computed(() => avatars.value.reverse())
+</script>
