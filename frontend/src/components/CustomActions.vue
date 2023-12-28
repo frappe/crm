0bec38a321cfc30d8f97f99302cd14bfde739081
Shariq Ansari <sharique.rik@gmail.com>
2023-12-28 16:32:16 +0530

    fix: added basic custom actions component

---
+++ frontend/src/components/CustomActions.vue
@@ -0,0 +1,16 @@
+<template>
+  <Button
+    v-for="action in actions"
+    :label="action.label"
+    @click="action.onClick()"
+  />
+</template>
+
+<script setup>
+const props = defineProps({
+  actions: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
