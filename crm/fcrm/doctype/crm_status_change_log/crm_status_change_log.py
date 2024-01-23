4305c97a83d3ed9c1dac773ffc2046fb88287ece
Shariq Ansari <sharique.rik@gmail.com>
2024-01-23 15:33:43 +0530

    fix: status change log fix

--- crm/fcrm/doctype/crm_status_change_log/crm_status_change_log.py
+++ crm/fcrm/doctype/crm_status_change_log/crm_status_change_log.py
@@ -3,6 +3,7 @@
 
 import frappe
 from datetime import datetime
+from frappe.utils import add_to_date
 from frappe.model.document import Document
 
 
@@ -12,6 +13,16 @@ class CRMStatusChangeLog(Document):
 
 def add_status_change_log(doc):
 	if not doc.is_new():
+		previous_status = doc.get_doc_before_save().status if doc.get_doc_before_save() else None
+		if not doc.status_change_log and previous_status:
+			now_minus_one_minute = add_to_date(datetime.now(), minutes=-1)
+			doc.append("status_change_log", {
+				"from": previous_status,
+				"to": "",
+				"from_date": now_minus_one_minute,
+				"to_date": "",
+				"log_owner": frappe.session.user,
+			})
 		last_status_change = doc.status_change_log[-1]
 		last_status_change.to = doc.status
 		last_status_change.to_date = datetime.now()
