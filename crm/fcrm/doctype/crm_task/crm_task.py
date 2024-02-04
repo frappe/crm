531e5710b8f5e34776fe21c680a7b741e3ad03e4
Shariq Ansari <sharique.rik@gmail.com>
2024-02-04 16:56:52 +0530

    fix: added task listview and dialog

--- crm/fcrm/doctype/crm_task/crm_task.py
+++ crm/fcrm/doctype/crm_task/crm_task.py
@@ -6,4 +6,55 @@ from frappe.model.document import Document
 
 
 class CRMTask(Document):
-	pass
+	@staticmethod
+	def default_list_data():
+		columns = [
+			{
+				'label': 'Title',
+				'type': 'Data',
+				'key': 'title',
+				'width': '12rem',
+			},
+			{
+				'label': 'Status',
+				'type': 'Select',
+				'key': 'status',
+				'width': '8rem',
+			},
+			{
+				'label': 'Priority',
+				'type': 'Select',
+				'key': 'priority',
+				'width': '8rem',
+			},
+			{
+				'label': 'Due Date',
+				'type': 'Date',
+				'key': 'due_date',
+				'width': '8rem',
+			},
+			{
+				'label': 'Assigned To',
+				'type': 'Link',
+				'key': 'assigned_to',
+				'width': '10rem',
+			},
+			{
+				'label': 'Last Modified',
+				'type': 'Datetime',
+				'key': 'modified',
+				'width': '8rem',
+			},
+		]
+
+		rows = [
+			"name",
+			"title",
+			"description",
+			"assigned_to",
+			"due_date",
+			"status",
+			"priority",
+			"modified",
+		]
+		return {'columns': columns, 'rows': rows}
