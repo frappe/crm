c60998a1fcc7b3b40bf5993fd64bf44ef75f434c
Shariq Ansari <sharique.rik@gmail.com>
2024-02-22 20:43:20 +0530

    fix: added send_message api to send message

--- crm/fcrm/doctype/crm_whatsapp_settings/crm_whatsapp_settings.js
+++ crm/fcrm/doctype/crm_whatsapp_settings/crm_whatsapp_settings.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("CRM Whatsapp Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("CRM Whatsapp Settings", {
+	refresh(frm) {
+		// add send whatsapp message button
+		frm.add_custom_button(__("Send Message"), () => {
+			frappe.call("crm.integrations.whatsapp.api.send_message", {
+				data: "",
+				to: "917666980887",
+			});
+		});
+	},
+});
--- crm/integrations/whatsapp/api.py
+++ crm/integrations/whatsapp/api.py
@@ -1,7 +1,8 @@
-import frappe
 import json
+import frappe
 from werkzeug.wrappers import Response
 
+from frappe.integrations.utils import make_post_request
 
 def whatsapp_settings():
     """Get whatsapp settings."""
@@ -31,9 +32,9 @@ def post():
     """Post."""
     data = frappe.local.form_dict
     try:
-        frappe.get_doc({"doctype": "CRM Whatsapp Log", "data": json.dumps(data)}).insert(
-            ignore_permissions=True
-        )
+        frappe.get_doc(
+            {"doctype": "CRM Whatsapp Log", "data": json.dumps(data, indent=2)}
+        ).insert(ignore_permissions=True)
 
         messages = data["entry"][0]["changes"][0]["value"].get("messages", [])
 
@@ -49,8 +50,64 @@ def post():
                             "message_id": message["id"],
                             "message_type": message_type,
                             "message": message["text"]["body"],
+                            "data": json.dumps(message, indent=2),
                         }
                     ).insert(ignore_permissions=True)
     except Exception as e:
         frappe.log_error(e)
         return Response("Error", status=500)
+
+
+@frappe.whitelist()
+def send_message(data, to):
+    whatsapp_settings = frappe.get_cached_doc("CRM Whatsapp Settings")
+    url = whatsapp_settings.url
+    token = whatsapp_settings.get_password("token")
+    phone_no_id = whatsapp_settings.phone_no_id
+    version = whatsapp_settings.version
+
+    data = {
+        "messaging_product": "whatsapp",
+        "to": to,
+        "type": "template",
+        "template": {
+            "name": "hello_world",
+            "language": {"code": "en_US"},
+        },
+    }
+    message = data["template"]
+    type = "Template"
+    message_type = "template"
+    # data = {
+    #     "messaging_product": "whatsapp",
+    #     "to": to,
+    #     "text": {
+    #         "body": "New World",
+    #     },
+    # }
+    # message = data["text"]["body"]
+    # type = "Manual"
+    # message_type = "text"
+    headers = {"authorization": f"Bearer {token}", "content-type": "application/json"}
+
+    try:
+        response = make_post_request(
+            f"{url}/{version}/{phone_no_id}/messages",
+            headers=headers,
+            data=json.dumps(data),
+        )
+        frappe.get_doc(
+            {
+                "doctype": "CRM Whatsapp Message",
+                "direction": "Outgoing",
+                "message": str(message),
+                "type": type,
+                "to": to,
+                "message_id": response["messages"][0]["id"],
+                "message_type": message_type,
+                "data": json.dumps(response, indent=2),
+            }
+        ).save(ignore_permissions=True)
+    except Exception as e:
+        frappe.log_error(e)
+        return None
