e3ff8657efba764c1a014e73b2339c6e7f0d98c6
Shariq Ansari <sharique.rik@gmail.com>
2024-03-16 18:28:41 +0530

    patch: move data from crm note to fcrm note

--- crm/patches/v1_0/rename_crm_note_to_fcrm_note.py
+++ crm/patches/v1_0/rename_crm_note_to_fcrm_note.py
@@ -11,3 +11,19 @@ def execute():
 	frappe.flags.ignore_route_conflict_validation = False
 
 	frappe.reload_doctype("FCRM Note", force=True)
+
+	notes = frappe.db.sql("SELECT * FROM `tabCRM Note`", as_dict=True)
+	if notes:
+		for note in notes:
+			doc = frappe.get_doc({
+				"doctype": "FCRM Note",
+				"creation": note.get("creation"),
+				"modified": note.get("modified"),
+				"modified_by": note.get("modified_by"),
+				"owner": note.get("owner"),
+				"title": note.get("title"),
+				"content": note.get("content"),
+				"reference_doctype": note.get("reference_doctype"),
+				"reference_docname": note.get("reference_docname"),
+			})
+			doc.db_insert()
