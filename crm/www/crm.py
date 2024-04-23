a9e59ff431e095899a102f0d97cc28bb05ea69ae
Shariq Ansari <sharique.rik@gmail.com>
2024-04-23 19:55:21 +0530

    fix: updated socket io implementation similar to gameplan

--- crm/www/crm.py
+++ crm/www/crm.py
@@ -14,3 +14,20 @@ def get_context(context):
     if frappe.session.user != "Guest":
         capture("active_site", "crm")
     context.csrf_token = csrf_token
+
+@frappe.whitelist(methods=['POST'], allow_guest=True)
+def get_context_for_dev():
+	if not frappe.conf.developer_mode:
+		frappe.throw('This method is only meant for developer mode')
+	return get_boot()
+
+def get_boot():
+	return frappe._dict({
+		'frappe_version': frappe.__version__,
+		'default_route': get_default_route(),
+		'site_name': frappe.local.site,
+		'read_only_mode': frappe.flags.read_only,
+	})
+
+def get_default_route():
+	return '/crm'
