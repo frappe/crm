eb08d598161beac359f1ff99d702f9c66d31e09d
Shariq Ansari <sharique.rik@gmail.com>
2024-04-24 16:00:29 +0530

    fix: added is twilio enabled check in settings store

--- frontend/src/stores/settings.js
+++ frontend/src/stores/settings.js
@@ -2,12 +2,21 @@ import { createResource } from 'frappe-ui'
 import { ref } from 'vue'
 
 export const whatsappEnabled = ref(false)
-
 createResource({
   url: 'crm.api.whatsapp.is_whatsapp_enabled',
   cache: 'Is Whatsapp Enabled',
   auto: true,
   onSuccess: (data) => {
     whatsappEnabled.value = data
+  },
+})
+
+export const twilioEnabled = ref(false)
+createResource({
+  url: 'crm.integrations.twilio.api.is_enabled',
+  cache: 'Is Twilio Enabled',
+  auto: true,
+  onSuccess: (data) => {
+    twilioEnabled.value = data
   },
 })
