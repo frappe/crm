9bd5e21b1bfc93e7b6af5a5808e4a36cc329e4f1
Shariq Ansari <sharique.rik@gmail.com>
2024-05-04 16:36:03 +0530

    fix: removed CallLog page

--- crm/fcrm/doctype/crm_call_log/crm_call_log.py
+++ crm/fcrm/doctype/crm_call_log/crm_call_log.py
@@ -78,60 +78,6 @@ class CRMCallLog(Document):
 			return {'columns': columns, 'rows': rows}
 
 @frappe.whitelist()
-def get_call_log(name):
-	doc = frappe.get_doc("CRM Call Log", name)
-	doc = doc.as_dict()
-	if doc.reference_docname and doc.reference_doctype == "CRM Lead":
-		doc.lead = doc.reference_docname
-		doc.lead_name = frappe.db.get_value("CRM Lead", doc.reference_docname, "lead_name")
-	if doc.note:
-		note = frappe.db.get_values("FCRM Note", doc.note, ["title", "content"])[0]
-		doc.note_doc = {
-			"name": doc.note,
-			"title": note[0],
-			"content": note[1]
-		}
-
-	def get_contact(number):
-		c = frappe.db.get_value("Contact", {"mobile_no": number}, ["full_name", "image"], as_dict=True)
-		if c:
-			return [c.full_name, c.image]
-		return [None, None]
-
-	def get_lead_contact(number):
-		l = frappe.db.get_value("CRM Lead", {"mobile_no": number, "converted": 0}, ["lead_name", "image"], as_dict=True)
-		if l:
-			return [l.lead_name, l.image]
-		return [None, None]
-
-	def get_user(user):
-		u = frappe.db.get_value("User", user, ["full_name", "user_image"], as_dict=True)
-		if u:
-			return [u.full_name, u.user_image]
-		return [None, None]
-
-	if doc.type == "Incoming":
-		doc.caller = {
-			"label": get_contact(doc.get("from"))[0] or get_lead_contact(doc.get("from"))[0] or "Unknown",
-			"image": get_contact(doc.get("from"))[1] or get_lead_contact(doc.get("from"))[1]
-		}
-		doc.receiver = {
-			"label": get_user(doc.get("receiver"))[0],
-			"image": get_user(doc.get("receiver"))[1]
-		}
-	else:
-		doc.caller = {
-			"label": get_user(doc.get("caller"))[0],
-			"image": get_user(doc.get("caller"))[1]
-		}
-		doc.receiver = {
-			"label": get_contact(doc.get("to"))[0] or get_lead_contact(doc.get("to"))[0] or "Unknown",
-			"image": get_contact(doc.get("to"))[1] or get_lead_contact(doc.get("to"))[1]
-	}
-
-	return doc
-
-@frappe.whitelist()
 def create_lead_from_call_log(call_log):
 	lead = frappe.new_doc("CRM Lead")
 	lead.first_name = "Lead from call " + call_log.get("from")
