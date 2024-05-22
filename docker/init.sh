1be19fbd4b0bbaab78411752e85b9e7ff4a2a0fe
Shariq Ansari <sharique.rik@gmail.com>
2024-05-22 20:26:00 +0530

    fix: added docker compose files

---
+++ docker/init.sh
@@ -0,0 +1,40 @@
+#!bin/bash
+
+if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
+    echo "Bench already exists, skipping init"
+    cd frappe-bench
+    bench start
+else
+    echo "Creating new bench..."
+fi
+
+bench init --skip-redis-config-generation frappe-bench
+
+cd frappe-bench
+
+# Use containers instead of localhost
+bench set-mariadb-host mariadb
+bench set-redis-cache-host redis:6379
+bench set-redis-queue-host redis:6379
+bench set-redis-socketio-host redis:6379
+
+# Remove redis, watch from Procfile
+sed -i '/redis/d' ./Procfile
+sed -i '/watch/d' ./Procfile
+
+bench get-app crm
+
+bench new-site crm.localhost \
+    --force \
+    --mariadb-root-password 123 \
+    --admin-password admin \
+    --no-mariadb-socket
+
+bench --site crm.localhost install-app crm
+bench --site crm.localhost set-config developer_mode 1
+bench --site crm.localhost clear-cache
+bench --site crm.localhost set-config mute_emails 1
+bench --site crm.localhost add-user alex@example.com --first-name Alex --last-name Scott --password 123 --user-type 'System User' --add-role 'crm Admin'
+bench use crm.localhost
+
+bench start
