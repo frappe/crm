4fb1ea240d6eaa1604b9c35ea12040c6febe590d
Shariq Ansari <sharique.rik@gmail.com>
2024-05-26 13:09:17 +0530

    feat: separated desktop & mobile layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <router-view v-if="$route.name == 'Login'" />
-  <DesktopLayout v-else-if="session().isLoggedIn">
+  <Layout v-else-if="session().isLoggedIn">
     <router-view />
-  </DesktopLayout>
+  </Layout>
   <Dialogs />
   <Toasts />
 </template>
 
 <script setup>
-import DesktopLayout from '@/components/Layouts/DesktopLayout.vue'
 import { Dialogs } from '@/utils/dialogs'
 import { sessionStore as session } from '@/stores/session'
+import { useScreenSize } from '@/composables'
 import { Toasts } from 'frappe-ui'
+import { computed, defineAsyncComponent } from 'vue'
+
+const screenSize = useScreenSize()
+const MobileLayout = defineAsyncComponent(() =>
+  import('./components/Layouts/MobileLayout.vue')
+)
+const DesktopLayout = defineAsyncComponent(() =>
+  import('./components/Layouts/DesktopLayout.vue')
+)
+const Layout = computed(() => {
+  if (screenSize.width < 640) {
+    return MobileLayout
+  } else {
+    return DesktopLayout
+  }
+})
 </script>
