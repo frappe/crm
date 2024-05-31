63020e6e8a12c8751aa1176fb2dd08bcfc9d8a9c
Shariq Ansari <sharique.rik@gmail.com>
2024-05-31 18:11:09 +0530

    fix: created bare minimum group by component

---
+++ frontend/src/components/GroupBy.vue
@@ -0,0 +1,40 @@
+<template>
+  <Dropdown :options="options">
+    <template #default="{ open }">
+      <Button :label="__('Group By: Status')">
+        <template #prefix>
+          <DetailsIcon />
+        </template>
+        <template #suffix>
+          <FeatherIcon
+            :name="open ? 'chevron-up' : 'chevron-down'"
+            class="h-4"
+          />
+        </template>
+      </Button>
+    </template>
+  </Dropdown>
+</template>
+<script setup>
+import DetailsIcon from '@/components/icons/DetailsIcon.vue'
+import { Dropdown } from 'frappe-ui';
+import { ref } from 'vue'
+
+const props = defineProps({
+  doctype: {
+    type: String,
+    required: true,
+  },
+})
+
+const list = defineModel()
+
+const selected = ref('Status')
+
+const options = ref([
+  { label: 'Status', value: 'status' },
+  { label: 'Source', value: 'source' },
+  { label: 'Owner', value: 'owner' },
+  { label: 'Created At', value: 'created_at' },
+])
+</script>
