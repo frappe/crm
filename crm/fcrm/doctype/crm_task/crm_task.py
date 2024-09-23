a9318d71c02b07a5adc8b8d485cb7ca852679e7d
Shariq Ansari <sharique.rik@gmail.com>
2024-09-23 21:14:39 +0530

    fix: show assigned & unassigned notification for Lead, Deal and Task

--- crm/fcrm/doctype/crm_task/crm_task.py
+++ crm/fcrm/doctype/crm_task/crm_task.py
@@ -4,7 +4,7 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.desk.form.assign_to import add as assign
+from frappe.desk.form.assign_to import add as assign, remove as unassign
 from crm.fcrm.doctype.crm_notification.crm_notification import notify_user
 
 
@@ -31,34 +31,6 @@ class CRMTask(Document):
 				"name": self.name,
 				"description": self.title or self.description,
 			})
-			self.notify_assigned_user()
-
-	def notify_assigned_user(self):
-		"""
-		Notify the assigned user about the task assignment
-		"""
-
-		owner = frappe.get_cached_value("User", self.owner, "full_name")
-		notification_text = f"""
-			<div class="mb-2 leading-5 text-gray-600">
-				<span class="font-medium text-gray-900">{ owner }</span>
-				<span>{ _('assigned a new task {0} to you').format(
-					f'<span class="font-medium text-gray-900">{ self.title }</span>'
-				) }</span>
-			</div>
-		"""
-
-		notify_user({
-			"owner": self.owner,
-			"assigned_to": self.assigned_to,
-			"notification_type": "Task",
-			"message": self.description,
-			"notification_text": notification_text,
-			"doctype": self.doctype,
-			"name": self.name,
-			"reference_doctype": self.reference_doctype,
-			"reference_docname": self.reference_docname,
-		})
 
 
 	@staticmethod
