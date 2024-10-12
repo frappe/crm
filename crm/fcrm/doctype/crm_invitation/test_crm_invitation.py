0d46df878e1c3339bd75634505c2bf2645cb8a09
Shariq Ansari <sharique.rik@gmail.com>
2024-10-12 12:50:41 +0530

    test: fixed failing unit test

--- crm/fcrm/doctype/crm_invitation/test_crm_invitation.py
+++ crm/fcrm/doctype/crm_invitation/test_crm_invitation.py
@@ -2,8 +2,8 @@
 # See license.txt
 
 # import frappe
-from frappe.tests.utils import FrappeTestCase
+from frappe.tests import UnitTestCase
 
 
-class TestCRMInvitation(FrappeTestCase):
+class TestCRMInvitation(UnitTestCase):
 	pass
