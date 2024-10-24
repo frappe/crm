a590460a53a106ccd8717f1e90d732173e8dc275
Anuradha Kalaskar <anuradha@zirrusone.com>
2024-10-24 19:26:23 +0530

    [WIP][need to follow up for deal report]

---
+++ crm/fcrm/report/needs_email_follow_up_for_deal/needs_email_follow_up_for_deal.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Needs Email Follow Up For Deal"] = {
+	"filters": [
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			reqd: 1,
+			default: frappe.datetime.add_days(frappe.datetime.nowdate(), -7),
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			reqd: 1,
+			default: frappe.datetime.nowdate()
+		},
+		{
+			fieldname: "assigned_to",
+			label: __("Assigned To"),
+			fieldtype: "Link",
+			options: "User",
+		    default:frappe.session.user
+		},
+		{
+			"fieldname": "status",
+			"label": "Status",
+            "fieldtype": "Link",
+            "options": 	"CRM Deal Status"
+		},
+
+	]
+};
