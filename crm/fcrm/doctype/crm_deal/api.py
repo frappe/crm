26df363f7f67d0c92e92fd53268a483612c56fc7
Shariq Ansari <sharique.rik@gmail.com>
2024-11-13 19:55:01 +0530

    fix: use get_doc to get lead/deal data

--- crm/fcrm/doctype/crm_deal/api.py
+++ crm/fcrm/doctype/crm_deal/api.py
@@ -1,33 +1,12 @@
 import frappe
-from frappe import _
 
 from crm.api.doc import get_fields_meta, get_assigned_users
 from crm.fcrm.doctype.crm_form_script.crm_form_script import get_form_script
 
 @frappe.whitelist()
 def get_deal(name):
-	Deal = frappe.qb.DocType("CRM Deal")
+	deal = frappe.get_doc("CRM Deal", name).as_dict()
 
-	query = (
-		frappe.qb.from_(Deal)
-		.select("*")
-		.where(Deal.name == name)
-		.limit(1)
-	)
-
-	deal = query.run(as_dict=True)
-	if not len(deal):
-		frappe.throw(_("Deal not found"), frappe.DoesNotExistError)
-	deal = deal.pop()
-
-
-	deal["contacts"] = frappe.get_all(
-		"CRM Contacts",
-		filters={"parenttype": "CRM Deal", "parent": deal.name},
-		fields=["contact", "is_primary"],
-	)
-
-	deal["doctype"] = "CRM Deal"
 	deal["fields_meta"] = get_fields_meta("CRM Deal") 
 	deal["_form_script"] = get_form_script('CRM Deal')
 	deal["_assign"] = get_assigned_users("CRM Deal", deal.name, deal.owner)
--- crm/fcrm/doctype/crm_lead/api.py
+++ crm/fcrm/doctype/crm_lead/api.py
@@ -6,16 +6,8 @@ from crm.fcrm.doctype.crm_form_script.crm_form_script import get_form_script
 
 @frappe.whitelist()
 def get_lead(name):
-	Lead = frappe.qb.DocType("CRM Lead")
+	lead = frappe.get_doc("CRM Lead", name).as_dict()
 
-	query = frappe.qb.from_(Lead).select("*").where(Lead.name == name).limit(1)
-
-	lead = query.run(as_dict=True)
-	if not len(lead):
-		frappe.throw(_("Lead not found"), frappe.DoesNotExistError)
-	lead = lead.pop()
-
-	lead["doctype"] = "CRM Lead"
 	lead["fields_meta"] = get_fields_meta("CRM Lead")
 	lead["_form_script"] = get_form_script('CRM Lead')
 	lead["_assign"] = get_assigned_users("CRM Lead", lead.name, lead.owner)
