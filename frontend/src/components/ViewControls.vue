fe9b0e1019215f41cd18a88c761574541747eb87
JoshuaR <36710177+joshuarestivo@users.noreply.github.com>
2024-11-14 15:37:09 +0000

    fix: [default deal status filter] Update statuses to reflect new defaults

--- frontend/src/components/ViewControls.vue
+++ frontend/src/components/ViewControls.vue
@@ -1189,7 +1189,7 @@ function applyLikeFilter() {
 
 function applyDefaultStatusFilter(value) {
     let filters = { ...list.value.params?.filters }
-  filters['status'] = ['not in', `Won, Lost`]
+  filters['status'] = ['not in', `Closed`]
   updateFilter(filters)
 }
 
