2bf30f061a6481a4bd3dee9e1961552a8821c4f9
Aryan Kaushik <34372791+Aryan20@users.noreply.github.com>
2024-11-21 01:40:27 +0530

    Revert "chore: disable ERP Next integration setting page"

---
+++ frontend/src/components/Icons/ERPNextIcon.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 5C1 2.79086 2.79086 1 5 1H13C15.2091 1 17 2.79086 17 5V13C17 15.2091 15.2091 17 13 17H5C2.79086 17 1 15.2091 1 13V5Z"
+        stroke="currentColor"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M11.7819 6.27142H11.5136H8.02453H6.28001V4.84002H11.7819V6.27142ZM8.02451 9.62623V11.5944H11.8267V13.0258H6.27999V8.19484H8.02451H11.5135V9.62623H8.02451Z"
+        fill="currentColor"
+      />
+    </svg>
+  </template>
