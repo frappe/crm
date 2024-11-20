cc66b8e12d6eb5b79b5984352a773d6eec3136a6
Aryan Kaushik <aryankaushik2023@gmail.com>
2024-11-20 09:01:48 +0000

    fix: rename FCRM instances to NCRM

--- next_crm/ncrm/doctype/ncrm_settings/ncrm_settings.js
+++ next_crm/ncrm/doctype/ncrm_settings/ncrm_settings.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("FCRM Settings", {
+frappe.ui.form.on("NCRM Settings", {
 	// refresh(frm) {
 
 	// },
