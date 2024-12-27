6cc22d8795a5443cfc42d25b60b7e4040ee44c57
Shariq Ansari <sharique.rik@gmail.com>
2024-12-27 19:43:43 +0530

    fix: update standard dropdown items from hooks while install and after migrate

--- crm/fcrm/doctype/fcrm_settings/fcrm_settings.py
+++ crm/fcrm/doctype/fcrm_settings/fcrm_settings.py
@@ -3,6 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
+
 from crm.install import after_install
 
 
@@ -10,3 +11,28 @@ class FCRMSettings(Document):
 	@frappe.whitelist()
 	def restore_defaults(self, force=False):
 		after_install(force)
+
+
+def after_migrate():
+	sync_table("dropdown_items", "standard_dropdown_items")
+
+
+def sync_table(key, hook):
+	crm_settings = FCRMSettings("FCRM Settings")
+	existing_items = {d.name1: d for d in crm_settings.get(key)}
+	new_standard_items = {}
+
+	# add new items
+	count = 0  # maintain count because list may come from seperate apps
+	for item in frappe.get_hooks(hook):
+		if item.get("name1") not in existing_items:
+			crm_settings.append(key, item, count)
+		new_standard_items[item.get("name1")] = True
+		count += 1
+
+	# remove unused items
+	items = crm_settings.get(key)
+	items = [item for item in items if not (item.is_standard and (item.name1 not in new_standard_items))]
+	crm_settings.set(key, items)
+
+	crm_settings.save()
