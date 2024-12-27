73f227f76b88242ce4d355a102f03ee8c14ed660
Shariq Ansari <sharique.rik@gmail.com>
2024-12-27 21:47:07 +0530

    fix: created a settings store

---
+++ frontend/src/stores/settings.js
@@ -0,0 +1,28 @@
+import { createDocumentResource } from 'frappe-ui'
+import { reactive, ref } from 'vue'
+
+const settings = ref({})
+const brand = reactive({})
+
+export function getSettings() {
+  const _settings = createDocumentResource({
+    doctype: 'FCRM Settings',
+    name: 'FCRM Settings',
+    onSuccess: (data) => {
+      settings.value = data
+      brand.name = settings.value?.brand_name
+      brand.logo = settings.value?.brand_logo
+      brand.favicon = settings.value?.favicon
+      return data
+    },
+  })
+
+  if (!settings.value?.length) {
+    _settings.fetch()
+  }
+
+  return {
+    settings,
+    brand,
+  }
+}
