0006e5652a53ffee1a3dfbd8a88235170cbb6450
Vlad Iacob <vlad.iacob@hackajob.co>
2025-02-10 14:58:40 +0200

    ST-289: API to delete lead with the linked doctypes

---
+++ crm/api/lead.py
@@ -0,0 +1,21 @@
+import frappe
+
+
+@frappe.whitelist()
+def delete_lead_and_links(name):
+	frappe.delete_doc('CRM Lead', name)
+
+	linked_doctypes = {
+		'Communication': 'reference_name',
+		'Comment': 'reference_name',
+		'CRM Call Log': 'reference_docname',
+		'CRM Task': 'reference_docname',
+		'FCRM Note': 'reference_docname',
+		'File': 'attached_to_name'
+	}
+	for doctype, fieldname in linked_doctypes.items():
+		linked_docs = frappe.get_all(doctype, filters={fieldname: name})
+		for doc in linked_docs:
+			frappe.delete_doc(doctype, doc.name)
+
+	frappe.db.commit()
