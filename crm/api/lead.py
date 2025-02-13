0e3a7c4b98e3c083169399e39bea9d68c527bdde
Vlad Iacob <vlad.iacob@hackajob.co>
2025-02-13 23:40:05 +0200

    Fix deleting the fcrm notification also

--- crm/api/lead.py
+++ crm/api/lead.py
@@ -3,17 +3,22 @@ import frappe
 
 @frappe.whitelist()
 def delete_lead_and_links(name):
+	print(name)
 	linked_doctypes = {
 		'Communication': 'reference_name',
 		'Comment': 'reference_name',
-		'CRM Call Log': 'reference_docname',
-		'CRM Task': 'reference_docname',
-		'FCRM Note': 'reference_docname',
 		'File': 'attached_to_name',
 		'Dynamic Link': 'link_name',
+		'CRM Call Log': 'reference_docname',
+		'CRM Task': 'reference_docname',
+		'CRM Notification': 'reference_name',
+		'FCRM Note': 'reference_docname',
 	}
 	for doctype, fieldname in linked_doctypes.items():
+		print(doctype)
+		print(fieldname)
 		linked_docs = frappe.get_all(doctype, filters={fieldname: name})
+		print(linked_docs)
 		for doc in linked_docs:
 			frappe.delete_doc(doctype, doc.name)
 
