7213cdc2bfc12f2a10c3dd8de383234254a4dfbd
Shariq Ansari <sharique.rik@gmail.com>
2025-02-19 11:42:06 +0530

    fix: set as default view

--- frontend/src/stores/views.js
+++ frontend/src/stores/views.js
@@ -42,6 +42,42 @@ export const viewsStore = defineStore('crm-views', (doctype) => {
     },
   })
 
+  function getDefaultView(routeName = false) {
+    let view = defaultView.data
+    if (!view) return null
+
+    if (typeof view === 'string' && !isNaN(view)) {
+      view = parseInt(view)
+    }
+
+    if (routeName) {
+      let viewObj = getView(view) || {
+        type: view.split('_')[0],
+        dt: view.split('_')[1],
+      }
+
+      let routeName = viewObj.dt
+
+      if (routeName.startsWith('CRM ')) {
+        routeName = routeName.slice(4)
+      }
+
+      if (!routeName.endsWith('s')) {
+        routeName += 's'
+      }
+
+      let viewName = viewObj.is_default ? null : viewObj.name
+
+      return {
+        name: routeName,
+        type: viewObj.type,
+        view: viewName,
+      }
+    }
+
+    return view
+  }
+
   function getView(view, type, doctype = null) {
     type = type || 'list'
     if (!view && doctype) {
@@ -68,6 +104,7 @@ export const viewsStore = defineStore('crm-views', (doctype) => {
     views,
     defaultView,
     standardViews,
+    getDefaultView,
     getPinnedViews,
     getPublicViews,
     reload,
