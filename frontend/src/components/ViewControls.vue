3029456be58d0214c4f6d2d3aad22b3934f2f733
Shariq Ansari <sharique.rik@gmail.com>
2025-03-02 14:32:29 +0530

    fix: show customize quick filter in kanban if manager

--- frontend/src/components/ViewControls.vue
+++ frontend/src/components/ViewControls.vue
@@ -193,7 +193,7 @@
           @update="(isDefault) => updateColumns(isDefault)"
         />
         <Dropdown
-          v-if="route.params.viewType !== 'kanban'"
+          v-if="route.params.viewType !== 'kanban' || isManager()"
           :options="[
             {
               group: __('Options'),
@@ -203,7 +203,7 @@
                   label: __('Export'),
                   icon: () => h(ExportIcon, { class: 'h-4 w-4' }),
                   onClick: () => (showExportDialog = true),
-                  condition: () => !options.hideColumnsButton,
+                  condition: () => !options.hideColumnsButton && route.params.viewType !== 'kanban',
                 },
                 {
                   label: __('Customize quick filters'),
