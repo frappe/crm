09881cc5251ddfff9f1814e8a3c80b6ae5088b60
Shariq Ansari <sharique.rik@gmail.com>
2025-03-19 02:43:58 +0530

    fix: updated tailwind config to include all vue files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
-    './node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}',
-    '../node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/frappe-ui/src/**/*.{vue,js,ts,jsx,tsx}',
+    '../node_modules/frappe-ui/src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   safelist: [{ pattern: /!(text|bg)-/, variants: ['hover', 'active'] }],
   theme: {
