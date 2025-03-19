5bba8459182ce1e139ee32058a32f1a901f85e0d
rajabxahmd <rajabxamd.tva@gmail.com>
2025-03-19 16:19:54 +0530

    Update frontend/src/pages/Tasks.vue
    Co-authored-by: Shariq Ansari <30859809+shariquerik@users.noreply.github.com>

--- frontend/src/pages/Tasks.vue
+++ frontend/src/pages/Tasks.vue
@@ -262,17 +262,11 @@ function parseRows(rows, columns = []) {
     tasks.value?.data.rows.forEach((row) => {
       _rows[row] = task[row]
 
-      let fieldType;
-      if (tasks.value.data.view_type === 'kanban') {
-        fieldType = columns?.find(
-          (col) => (col.fieldname || col.value) == row,
-        )?.fieldtype
-      }
-      else {
-        fieldType = columns?.find(
-          (col) => (col.key || col.value) == row,
-        )?.type
-      }
+      let fieldname = deals.value.data.view_type === 'kanban' ? 'fieldname' : 'key'
+      let fieldtype = deals.value.data.view_type === 'kanban' ? 'fieldtype' : 'type'
+      let fieldType = columns?.find(
+        (col) => (col[fieldname] || col.value) == row,
+      )?.[fieldtype]
 
       if (
         fieldType &&
