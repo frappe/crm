71f6682e686f937f2b5aed0912686d652ab1c862
Aryan Kaushik <aryankaushik2023@gmail.com>
2025-03-26 18:35:34 +0000

    Add primary contact feature to lead contacts

--- frontend/src/pages/Lead.vue
+++ frontend/src/pages/Lead.vue
@@ -293,6 +293,13 @@
                             <div class="truncate">
                               {{ contact.full_name }}
                             </div>
+                            <Badge
+                              v-if="contact.is_primary_contact"
+                              class="ml-2"
+                              variant="outline"
+                              :label="__('Primary')"
+                              theme="green"
+                            />
                           </div>
                           <div class="flex items-center">
                             <Dropdown :options="contactOptions(contact)">
@@ -658,6 +665,7 @@ const lead = createResource({
       deleteDoc: deleteLead,
       resource: {
         lead,
+        leadContacts,
         fieldsLayout,
       },
       call,
@@ -771,6 +779,15 @@ function contactOptions(contact) {
       onClick: () => removeContact(contact.name),
     },
   ]
+
+  if (!contact.is_primary_contact) {
+    options.push({
+      label: __('Set as Primary Contact'),
+      icon: h(SuccessIcon, { class: 'h-4 w-4' }),
+      onClick: () => setPrimaryContact(contact.name),
+    })
+  }
+
   return options
 }
 
@@ -842,6 +859,22 @@ async function removeAddress(address) {
     leadAddresses.reload()
     createToast({
       title: __('Address removed'),
+      icon: 'check',
+      iconClasses: 'text-ink-green-3',
+    })
+  }
+}
+
+async function setPrimaryContact(contact) {
+  let d = await call('next_crm.api.contact.set_primary_contact', {
+    doctype: "Lead",
+    docname: props.leadId,
+    contact,
+  })
+  if (d) {
+    leadContacts.reload()
+    createToast({
+      title: __('Primary contact set'),
       icon: 'check',
       iconClasses: 'text-ink-green-3',
     })
