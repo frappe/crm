61ba3bd7f9e5861056b71b44e2ca6cd5c1e8af0d
Shariq Ansari <sharique.rik@gmail.com>
2025-04-07 13:58:58 +0530

    fix: added multi action button
    (cherry picked from commit 3b432a0209689fbb8f92a6401816d88add8c0e5c)

---
+++ frontend/src/components/MultiActionButton.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="flex items-center">
+    <Button
+      variant="solid"
+      class="border-0 rounded-br-none rounded-tr-none"
+      :label="activeButton.label"
+      :size="$attrs.size"
+      :class="$attrs.class"
+      @click="() => activeButton.onClick()"
+    >
+      <template #prefix>
+        <component
+          v-if="activeButton.icon"
+          :is="activeButton.icon"
+          class="h-4 w-4"
+        />
+      </template>
+    </Button>
+    <Dropdown
+      :options="parsedOptions"
+      size="sm"
+      class="flex-1 [&>div>div>div]:w-full"
+      placement="right"
+    >
+      <template v-slot="{ togglePopover }">
+        <Button
+          variant="solid"
+          @click="togglePopover"
+          icon="chevron-down"
+          class="!w-6 justify-start rounded-bl-none rounded-tl-none border-0 pr-0 text-xs"
+        />
+      </template>
+    </Dropdown>
+  </div>
+</template>
+<script setup>
+import { Dropdown } from 'frappe-ui'
+import { computed, ref } from 'vue'
+
+const props = defineProps({
+  options: {
+    type: Array,
+    default: () => [],
+  },
+})
+
+const activeButton = ref(props.options?.[0] || {})
+
+const parsedOptions = computed(() => {
+  return (
+    props.options?.map((option) => {
+      return {
+        label: option.label,
+        onClick: () => {
+          activeButton.value = option
+        },
+      }
+    }) || []
+  )
+})
+</script>
