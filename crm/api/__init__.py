8a9361d822e10a27f630ba79d7b3393728f1fa68
Shariq Ansari <sharique.rik@gmail.com>
2025-04-08 16:01:23 +0530

    revert: module validation

--- crm/api/__init__.py
+++ crm/api/__init__.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from frappe.core.api.file import get_max_file_size
 from frappe.translate import get_all_translations
 from frappe.utils import cstr, split_emails, validate_email_address
-from frappe.utils.modules import get_modules_from_all_apps_for_user
 from frappe.utils.telemetry import POSTHOG_HOST_FIELD, POSTHOG_PROJECT_FIELD
 
 
@@ -63,11 +62,6 @@ def get_posthog_settings():
 def check_app_permission():
 	if frappe.session.user == "Administrator":
 		return True
-
-	allowed_modules = get_modules_from_all_apps_for_user()
-	allowed_modules = [x["module_name"] for x in allowed_modules]
-	if "FCRM" not in allowed_modules:
-		return False
 
 	roles = frappe.get_roles()
 	if any(
