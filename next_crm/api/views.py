22777941fc8bdd5deaa5e9ade97f1e9b8540490d
Aryan Kaushik <aryankaushik2023@gmail.com>
2025-04-09 22:39:24 +0000

    Add API t oget default view

--- next_crm/api/views.py
+++ next_crm/api/views.py
@@ -14,3 +14,39 @@ def get_views(doctype):
         query = query.where(View.dt == doctype)
     views = query.run(as_dict=True)
     return views
+
+
+@frappe.whitelist()
+def get_default_open_view():
+
+    views = frappe.get_list(
+        "CRM View Settings",
+        fields=["dt", "type", "user", "is_default", "default_open_view"],
+        filters={
+            "user": frappe.session.user,
+            "default_open_view": 1,
+            "is_default": 1,
+            "dt": ["is", "set"],
+        },
+    )
+
+    fallback_views = frappe.get_list(
+        "CRM View Settings",
+        fields=["dt", "type"],
+        filters={
+            "user": "",
+            "default_open_view": 1,
+            "is_default": 1,
+            "dt": ["is", "set"],
+        },
+    )
+
+    default_view_object = {}
+    for view in views:
+        default_view_object[view.dt] = view.type
+
+    for view in fallback_views:
+        if not default_view_object[view.dt]:
+            default_view_object[view.dt] = view.type
+
+    return default_view_object
