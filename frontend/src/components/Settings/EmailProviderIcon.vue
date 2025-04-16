7c4a697b9fc943a2ac3ffc8f83e41f0d2d2b6230
Pratik <badhepd@gmail.com>
2025-04-16 18:00:28 +0530

    fix: dark mode email account css
    (cherry picked from commit 9c45877999234e72fed113639b767fb7594b66f6)
    
    # Conflicts:
    #	frontend/src/components/Settings/EmailAccountCard.vue
    #	frontend/src/components/Settings/EmailAccountList.vue
    #	frontend/src/components/Settings/EmailAdd.vue
    #	frontend/src/components/Settings/EmailEdit.vue
    #	frontend/src/components/Settings/EmailProviderIcon.vue

---
+++ frontend/src/components/Settings/EmailProviderIcon.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="flex items-center justify-center w-8 h-8 bg-gray-100 cursor-pointer rounded-xl hover:bg-gray-200"
+    :class="{ 'ring-2 ring-gray-500 dark:ring-gray-100': selected }"
+  >
+    <img :src="logo" class="w-4 h-4" />
+  </div>
+  <p
+    v-if="serviceName"
+    class="text-xs text-center text-gray-700 dark:text-gray-500 mt-2"
+  >
+    {{ serviceName }}
+  </p>
+</template>
+
+<script setup>
+defineProps({
+  logo: {
+    type: String,
+    required: true,
+  },
+  serviceName: {
+    type: String,
+    default: '',
+  },
+  selected: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<style scoped></style>
