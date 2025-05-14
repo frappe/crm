00470770744f2590b84b8eafe4014250c5f1ec56
Shariq Ansari <sharique.rik@gmail.com>
2025-05-14 19:01:43 +0530

    feat: enhance FormattedInput component with description slot and useAttrs for better attribute handling

--- frontend/src/components/Controls/FormattedInput.vue
+++ frontend/src/components/Controls/FormattedInput.vue
@@ -6,10 +6,15 @@
     @blur="isFocused = false"
     v-bind="$attrs"
   />
+  <slot name="description">
+    <p v-if="attrs.description" class="mt-1.5" :class="descriptionClasses">
+      {{ attrs.description }}
+    </p>
+  </slot>
 </template>
 <script setup>
 import { TextInput } from 'frappe-ui'
-import { ref, computed, nextTick } from 'vue'
+import { ref, computed, nextTick, useAttrs } from 'vue'
 
 const props = defineProps({
   value: {
@@ -21,6 +26,8 @@ const props = defineProps({
     default: '',
   },
 })
+
+const attrs = useAttrs()
 
 const isFocused = ref(false)
 const inputRef = ref(null)
@@ -37,5 +44,15 @@ function handleFocus() {
 
 const displayValue = computed(() => {
   return isFocused.value ? props.value : props.formattedValue || props.value
+})
+
+const descriptionClasses = computed(() => {
+  return [
+    {
+      sm: 'text-xs',
+      md: 'text-base',
+    }[attrs.size || 'sm'],
+    'text-ink-gray-5',
+  ]
 })
 </script>
