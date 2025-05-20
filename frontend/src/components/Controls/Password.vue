3ea056d3df31e6b8f8cf62743953dab627de997c
Shariq Ansari <sharique.rik@gmail.com>
2025-05-20 13:23:26 +0530

    feat: added password control
    (cherry picked from commit fe8e309399d7f8f61e6a1388cf1820f07efc571f)

---
+++ frontend/src/components/Controls/Password.vue
@@ -0,0 +1,32 @@
+<template>
+  <FormControl
+    :type="show ? 'text' : 'password'"
+    :value="modelValue || value"
+    v-bind="$attrs"
+  >
+    <template #suffix>
+      <Button v-show="showEye" class="!h-4" @click="show = !show">
+        <FeatherIcon :name="show ? 'eye-off' : 'eye'" class="h-3" />
+      </Button>
+    </template>
+  </FormControl>
+</template>
+<script setup>
+import { FormControl } from 'frappe-ui'
+import { ref, computed } from 'vue'
+const props = defineProps({
+  modelValue: {
+    type: [String, Number],
+    default: '',
+  },
+  value: {
+    type: [String, Number],
+    default: '',
+  },
+})
+const show = ref(false)
+const showEye = computed(() => {
+  let v = props.modelValue || props.value
+  return !v?.includes('*')
+})
+</script>
