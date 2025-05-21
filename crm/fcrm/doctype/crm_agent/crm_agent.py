346849631eb2ac886b6bf796d355f3891217ff9f
Shariq Ansari <sharique.rik@gmail.com>
2025-05-21 14:55:09 +0530

    fix: show and allow changing role from agents settings page

--- crm/fcrm/doctype/crm_agent/crm_agent.py
+++ crm/fcrm/doctype/crm_agent/crm_agent.py
@@ -1,9 +1,27 @@
 # Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class CRMAgent(Document):
 	pass
+
+
+@frappe.whitelist()
+def update_agent_role(user, new_role):
+	"""
+	Update the role of the user to Agent
+	"""
+
+	user_doc = frappe.get_doc("User", user)
+
+	if new_role == "Sales Manager":
+		user_doc.append_roles("Sales Manager", "System Manager")
+	if new_role == "Sales User":
+		user_doc.append_roles("Sales User")
+		if "Sales Manager" in frappe.get_roles(user_doc.name):
+			user_doc.remove_roles("Sales Manager", "System Manager")
+
+	user_doc.save()
