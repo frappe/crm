029c16d1d0b71b83e108b479e077fbd168392870
Shariq Ansari <sharique.rik@gmail.com>
2025-06-13 14:46:44 +0530

    fix: circular import

--- crm/install.py
+++ crm/install.py
@@ -5,7 +5,6 @@ import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 from crm.fcrm.doctype.crm_products.crm_products import create_product_details_script
-from crm.fcrm.doctype.fcrm_settings.fcrm_settings import create_forecasting_script
 
 
 def before_install():
@@ -360,6 +359,8 @@ def add_standard_dropdown_items():
 
 
 def add_default_scripts():
+	from crm.fcrm.doctype.fcrm_settings.fcrm_settings import create_forecasting_script
+
 	for doctype in ["CRM Lead", "CRM Deal"]:
 		create_product_details_script(doctype)
 	create_forecasting_script()
