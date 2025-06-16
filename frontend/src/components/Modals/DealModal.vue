46ed866de67f2454195856963a58871344d87c99
Shariq Ansari <sharique.rik@gmail.com>
2025-06-16 17:42:01 +0530

    fix: updated button with icon
    (cherry picked from commit 9f3477e1cd43c7aa8144b661e7bed7ee7e8dd53b)

--- frontend/src/components/Modals/DealModal.vue
+++ frontend/src/components/Modals/DealModal.vue
@@ -15,10 +15,14 @@
               class="w-7"
               @click="openQuickEntryModal"
             >
-              <EditIcon class="h-4 w-4" />
+              <template #icon>
+                <EditIcon />
+              </template>
             </Button>
             <Button variant="ghost" class="w-7" @click="show = false">
-              <FeatherIcon name="x" class="h-4 w-4" />
+              <template #icon>
+                <FeatherIcon name="x" class="size-4" />
+              </template>
             </Button>
           </div>
         </div>
