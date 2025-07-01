08243530e7ff6b82c7d9f6e836a36b39bf03b9bf
Shariq Ansari <sharique.rik@gmail.com>
2025-07-01 15:06:02 +0530

    fix: mandatory error
    (cherry picked from commit 4c70b1a06bff81aac6baacffe34d624c7e3c4b86)

--- frontend/src/data/document.js
+++ frontend/src/data/document.js
@@ -26,7 +26,10 @@ export function useDocument(doctype, docname) {
             let errorMessage = __('Error updating document')
             if (err.exc_type == 'MandatoryError') {
               const fieldName = err.messages
-                .map((msg) => msg.split(': ')[2].trim())
+                .map((msg) => {
+                  let arr = msg.split(': ')
+                  return arr[arr.length - 1].trim()
+                })
                 .join(', ')
               errorMessage = __('Mandatory field error: {0}', [fieldName])
             }
