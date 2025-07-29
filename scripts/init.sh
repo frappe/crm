b6a6152a492d31e8fd0ce600c968d0002c6fda95
C.L.STARK <x.stark.dylan@gmail.com>
2025-07-30 05:11:40 +0800

    Update init.sh
    Fixed: docker setup error in crm-frappe container.

--- scripts/init.sh
+++ scripts/init.sh
@@ -26,9 +26,9 @@ cd frappe-bench
 
 # Use containers instead of localhost
 bench set-mariadb-host mariadb
-bench set-redis-cache-host redis-cache:6379
-bench set-redis-queue-host redis-queue:6379
-bench set-redis-socketio-host redis-socketio:6379
+bench set-redis-cache-host redis://redis-cache:6379
+bench set-redis-queue-host redis://redis-queue:6379
+bench set-redis-socketio-host redis://redis-socketio:6379
 
 # Remove redis from Procfile
 sed -i '/redis/d' ./Procfile
