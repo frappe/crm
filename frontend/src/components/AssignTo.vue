faef5cb86629ace8a113bd5dc06cfaa272d55e88
Shariq Ansari <sharique.rik@gmail.com>
2025-08-01 16:56:06 +0530

    refactor: update AssignTo component to use docname prop and streamline assignment logic

--- frontend/src/components/AssignTo.vue
+++ frontend/src/components/AssignTo.vue
@@ -13,9 +13,10 @@
       <AssignToBody
         v-show="open"
         v-model="assignees"
-        :doc="data"
+        :docname="docname"
         :doctype="doctype"
         :open="open"
+        :onUpdate="ownerField && saveAssignees"
       />
     </template>
   </NestedPopover>
@@ -24,11 +25,74 @@
 import NestedPopover from '@/components/NestedPopover.vue'
 import MultipleAvatar from '@/components/MultipleAvatar.vue'
 import AssignToBody from '@/components/AssignToBody.vue'
+import { useDocument } from '@/data/document'
+import { toast } from 'frappe-ui'
+import { computed } from 'vue'
 
 const props = defineProps({
-  data: Object,
   doctype: String,
+  docname: String,
 })
 
+const { document } = useDocument(props.doctype, props.docname)
+
 const assignees = defineModel()
+
+const ownerField = computed(() => {
+  if (props.doctype === 'CRM Lead') {
+    return 'lead_owner'
+  } else if (props.doctype === 'CRM Deal') {
+    return 'deal_owner'
+  } else {
+    return null
+  }
+})
+
+async function saveAssignees(
+  addedAssignees,
+  removedAssignees,
+  addAssignees,
+  removeAssignees,
+) {
+  removedAssignees.length && (await removeAssignees.submit(removedAssignees))
+  addedAssignees.length && (await addAssignees.submit(addedAssignees))
+
+  const nextAssignee = assignees.value.find(
+    (a) => a.name !== document.doc[ownerField.value],
+  )
+
+  let owner = ownerField.value.replace('_', ' ')
+
+  if (
+    document.doc[ownerField.value] &&
+    removedAssignees.includes(document.doc[ownerField.value])
+  ) {
+    document.doc[ownerField.value] = nextAssignee ? nextAssignee.name : ''
+    document.save.submit()
+
+    if (nextAssignee) {
+      toast.info(
+        __(
+          'Since you removed {0} from the assignee, the {0} has been changed to the next available assignee {1}.',
+          [owner, nextAssignee.label || nextAssignee.name],
+        ),
+      )
+    } else {
+      toast.info(
+        __(
+          'Since you removed {0} from the assignee, the {0} has also been removed.',
+          [owner],
+        ),
+      )
+    }
+  } else if (!document.doc[ownerField.value] && nextAssignee) {
+    document.doc[ownerField.value] = nextAssignee ? nextAssignee.name : ''
+    toast.info(
+      __('Since you added a new assignee, the {0} has been set to {1}.', [
+        owner,
+        nextAssignee.label || nextAssignee.name,
+      ]),
+    )
+  }
+}
 </script>
