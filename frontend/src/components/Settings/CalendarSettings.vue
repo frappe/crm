06f70bb42efcc229668b211be90acea24e64c7fa
Shariq Ansari <sharique.rik@gmail.com>
2025-09-26 15:39:03 +0530

    feat: add event and all-day event notification settings

--- frontend/src/components/Settings/CalendarSettings.vue
+++ frontend/src/components/Settings/CalendarSettings.vue
@@ -54,15 +54,216 @@
           />
         </div>
       </div>
+      <div class="h-px border-t mx-2 border-outline-gray-modals" />
+      <div class="flex flex-col gap-3 px-2 py-3">
+        <div>
+          <div class="text-p-base font-medium text-ink-gray-7 truncate">
+            {{ __('Event notifications') }}
+          </div>
+          <div
+            class="text-p-sm text-ink-gray-5"
+            v-html="
+              __(
+                'Reminders will be sent <b>before the event starts</b>, based on the configured time',
+              )
+            "
+          />
+        </div>
+        <div
+          v-if="notifications?.length"
+          class="rounded-lg flex flex-col gap-2 w-fit"
+        >
+          <div v-for="notification in notifications" :key="notification.name">
+            <div class="flex items-center gap-2">
+              <FormControl
+                class="w-36 shrink-0"
+                type="select"
+                :options="[
+                  {
+                    label: __('Notification'),
+                    value: 'Notification',
+                  },
+                  {
+                    label: __('Email'),
+                    value: 'Email',
+                  },
+                ]"
+                v-model="notification.type"
+                :placeholder="__('Notification')"
+              />
+              <FormControl
+                class="w-20 shrink-0"
+                type="number"
+                min="1"
+                v-model.number="notification.before"
+                :placeholder="__('10')"
+              />
+              <FormControl
+                class="w-32 shrink-0"
+                type="select"
+                :options="[
+                  {
+                    label:
+                      notification.before == 1 ? __('minute') : __('minutes'),
+                    value: 'minutes',
+                  },
+                  {
+                    label: notification.before == 1 ? __('hour') : __('hours'),
+                    value: 'hours',
+                  },
+                  {
+                    label: notification.before == 1 ? __('day') : __('days'),
+                    value: 'days',
+                  },
+                  {
+                    label: notification.before == 1 ? __('week') : __('weeks'),
+                    value: 'weeks',
+                  },
+                ]"
+                v-model="notification.interval"
+                :placeholder="__('minutes')"
+              />
+              <Button
+                icon="x"
+                variant="ghost"
+                @click="
+                  notifications.splice(notifications.indexOf(notification), 1)
+                "
+              />
+            </div>
+          </div>
+        </div>
+        <Button
+          class="w-fit"
+          :label="__('Add Notification')"
+          iconLeft="plus"
+          @click="
+            notifications.push({
+              type: 'Notification',
+              before: 10,
+              interval: 'minutes',
+            })
+          "
+        />
+      </div>
+      <div class="h-px border-t mx-2 border-outline-gray-modals" />
+      <div class="flex flex-col gap-3 py-3 px-2">
+        <div>
+          <div class="text-p-base font-medium text-ink-gray-7 truncate">
+            {{ __('All day event notifications') }}
+          </div>
+          <div
+            class="text-p-sm text-ink-gray-5"
+            v-html="
+              __(
+                'For all-day events, <b>set a time</b> to send reminders before the event starts',
+              )
+            "
+          />
+        </div>
+        <div
+          v-if="allDayNotifications?.length"
+          class="rounded-lg flex flex-col gap-2 w-fit"
+        >
+          <div
+            v-for="notification in allDayNotifications"
+            :key="notification.name"
+          >
+            <div class="flex items-center gap-2">
+              <FormControl
+                class="w-36 shrink-0"
+                type="select"
+                :options="[
+                  {
+                    label: __('Notification'),
+                    value: 'Notification',
+                  },
+                  {
+                    label: __('Email'),
+                    value: 'Email',
+                  },
+                ]"
+                v-model="notification.type"
+                :placeholder="__('Notification')"
+              />
+              <FormControl
+                class="w-20 shrink-0"
+                type="number"
+                min="1"
+                v-model.number="notification.before"
+                :placeholder="__('10')"
+              />
+              <FormControl
+                class="w-32 shrink-0"
+                type="select"
+                :options="[
+                  {
+                    label: notification.before == 1 ? __('day') : __('days'),
+                    value: 'days',
+                  },
+                  {
+                    label: notification.before == 1 ? __('week') : __('weeks'),
+                    value: 'weeks',
+                  },
+                ]"
+                v-model="notification.interval"
+                :placeholder="__('minutes')"
+              />
+              <div class="text-p-sm text-ink-gray-5">
+                {{ __('before at') }}
+              </div>
+              <TimePicker
+                class="w-32 shrink-0"
+                v-model="notification.time"
+                :placeholder="__('08:00 pm')"
+              />
+              <Button
+                icon="x"
+                variant="ghost"
+                @click="
+                  allDayNotifications.splice(
+                    allDayNotifications.indexOf(notification),
+                    1,
+                  )
+                "
+              />
+            </div>
+          </div>
+        </div>
+        <Button
+          class="w-fit"
+          :label="__('Add Notification')"
+          iconLeft="plus"
+          @click="
+            allDayNotifications.push({
+              type: 'Notification',
+              before: 1,
+              interval: 'days',
+              time: '08:00',
+            })
+          "
+        />
+      </div>
     </div>
   </div>
 </template>
 <script setup>
 import { getSettings } from '@/stores/settings'
 import { showSettings } from '@/composables/settings'
-import { FormControl } from 'frappe-ui'
+import { FormControl, TimePicker } from 'frappe-ui'
+import { computed } from 'vue'
 
 const { _settings: settings } = getSettings()
+
+const notifications = computed({
+  get: () => settings.doc.event_notifications || [],
+  set: (val) => (settings.doc.event_notifications = val),
+})
+
+const allDayNotifications = computed({
+  get: () => settings.doc.all_day_event_notifications || [],
+  set: (val) => (settings.doc.all_day_event_notifications = val),
+})
 
 function updateSettings() {
   settings.save.submit(null, {
