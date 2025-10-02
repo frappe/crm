91982d075760244e8e6e1a822aab3dadb6ca0e24
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-10-02 12:59:45 +0530

    feat: create new sync source
    (cherry picked from commit b12c55531f261f603b6bbb828c33a61a3c73503e)

--- frontend/src/components/Settings/LeadSyncing/NewLeadSyncSource.vue
+++ frontend/src/components/Settings/LeadSyncing/NewLeadSyncSource.vue
@@ -8,45 +8,99 @@
                 {{ __('Choose the type of source you want to configure.') }}
             </p>
         </div>
-        
+
         <!-- supported sources -->
         <div class="flex flex-wrap items-center">
-            <div
-                v-for="s in supportedSourceTypes"
-                :key="s.name"
-                class="flex flex-col items-center gap-1 mt-4 w-[70px]"
-                @click="handleSelect(s)"
-            >
-                <EmailProviderIcon
-                    :label="s.name"
-                    :logo="s.icon"
-                    :selected="selectedSourceType?.name === s?.name"
-                />
+            <div v-for="s in supportedSourceTypes" :key="s.name" class="flex flex-col items-center gap-1 mt-4 w-[70px]"
+                @click="handleSelect(s)">
+                <EmailProviderIcon :label="s.name" :logo="s.icon" :selected="selectedSourceType?.name === s?.name" />
             </div>
+        </div>
+
+        <div v-if="selectedSourceType" class="flex flex-col gap-4">
+            <!-- docs -->
+            <div class="flex items-center gap-2 p-2 rounded-md ring-1 ring-outline-gray-3 text-ink-gray-6">
+                <CircleAlert class="w-5 h-6 w-min-5 w-max-5 min-h-5 max-w-5" />
+                <div class="text-xs text-wrap">
+                    {{ selectedSourceType.info }}
+                    <a :href="selectedSourceType.link" target="_blank" class="underline">
+                        {{ __('here') }}
+                    </a>.
+                </div>
+            </div>
+
+            <!-- Form -->
+            <div v-if="selectedSourceType.name === 'Facebook'" class="flex flex-col gap-4">
+                <div class="grid grid-cols-1 gap-4">
+                    <div v-for="field in fbSourceFields" :key="field.name" class="flex flex-col gap-1">
+                        <FormControl v-model="state[field.name]" :label="field.label" :name="field.name"
+                            :type="field.type" :placeholder="field.placeholder" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- action button -->
+        <div v-if="selectedSourceType" class="flex justify-between mt-auto">
+            <Button :label="__('Back')" variant="outline" :disabled="sources.insert.loading"
+                @click="emit('updateStep', 'source-list')" />
+            <Button :label="__('Create')" variant="solid" :loading="sources.insert.loading"
+                @click="createLeadSyncSource" />
         </div>
     </div>
 </template>
 
 
 <script setup>
- import { computed, reactive, ref } from 'vue'
-import { createResource, toast } from 'frappe-ui'
-import CircleAlert from '~icons/lucide/circle-alert'
-// import {
-//   customProviderFields,
-//   popularProviderFields,
-//   services,
-//   validateInputs,
-//   incomingOutgoingFields,
-// } from './emailConfig'
-import { supportedSourceTypes } from './leadSyncSourceConfig'
-import EmailProviderIcon from '../EmailProviderIcon.vue'
+import { reactive, ref, inject } from "vue";
+import { createResource, FormControl, toast } from "frappe-ui";
+import CircleAlert from "~icons/lucide/circle-alert";
+import { supportedSourceTypes } from "./leadSyncSourceConfig";
+import EmailProviderIcon from "../EmailProviderIcon.vue";
 
+const state = reactive({
+    name: "",
+    type: "",
+    access_token: "",
+});
 
-const selectedSourceType = ref(null)
+const emit = defineEmits()
+
+const selectedSourceType = ref(supportedSourceTypes[0]);
+state.type = selectedSourceType.value.name;
+
+const sources = inject("sources");
+const fbSourceFields = [
+    {
+        name: "name",
+        label: __("Name"),
+        type: "text",
+        placeholder: __("Add a name for your source"),
+    },
+    {
+        name: "access_token",
+        label: __("Access Token"),
+        type: "password",
+        placeholder: __("Enter your Facebook Access Token"),
+    },
+];
 
 function handleSelect(sourceType) {
-  selectedSourceType.value = sourceType
-//   state.service = service.name
+    selectedSourceType.value = sourceType;
+    state.type = sourceType.name;
+}
+
+function createLeadSyncSource() {
+    sources.insert.submit({
+        ...state
+    }, {
+        onSuccess: () => {
+            toast.success(__('New Lead Syncing Source created successfully'))
+            emit('updateStep', 'source-list')
+        },
+        onError: (error) => {
+            toast.error(error.messages[0] || __('Failed to create source'))
+        },
+    })
 }
 </script>
