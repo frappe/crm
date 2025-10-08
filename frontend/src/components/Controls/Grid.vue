d69b86bcaf7e5d5dff9718637a2434df6f599f41
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-10-08 12:06:09 +0530

    feat(Grid): support for field overrides and Autocomplete

--- frontend/src/components/Controls/Grid.vue
+++ frontend/src/components/Controls/Grid.vue
@@ -271,6 +271,15 @@
                     :disabled="Boolean(field.read_only)"
                     @change="fieldChange(flt($event.target.value), field, row)"
                   />
+                  <Autocomplete
+                    v-else-if="field.fieldtype === 'Autocomplete'"
+                    class="text-sm text-ink-gray-8"
+                    v-model="row[field.fieldname]"
+                    @change="(s) => fieldChange(s.value, field, row)"
+                    :options="field.options"
+                    :placeholder="field.placeholder"
+                    :disabled="Boolean(field.read_only)"
+                  />
                   <FormControl
                     v-else
                     class="text-sm text-ink-gray-8"
@@ -360,6 +369,7 @@ import {
   DatePicker,
   Tooltip,
   dayjs,
+  Autocomplete
 } from 'frappe-ui'
 import Draggable from 'vuedraggable'
 import { ref, reactive, computed, inject, provide } from 'vue'
@@ -381,6 +391,10 @@ const props = defineProps({
     type: String,
     required: true,
   },
+  overrides: {
+    type: Object,
+    default: () => ({}),
+  }
 })
 
 const triggerOnChange = inject('triggerOnChange', () => {})
@@ -449,10 +463,17 @@ function getFieldObj(field) {
     })
   }
 
-  return {
+  const fieldObjWithFilters ={
     ...field,
     filters: field.link_filters && JSON.parse(field.link_filters),
     placeholder: field.placeholder || field.label,
+  }
+  
+  return {
+    ...fieldObjWithFilters,
+    ...props.overrides.fields?.find(
+      (f) => f.fieldname === field.fieldname,
+    ),
   }
 }
 
