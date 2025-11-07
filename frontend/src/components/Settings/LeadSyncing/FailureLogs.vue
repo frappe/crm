25b37157f48c2d2d9d55428f43df59726ebfcfbf
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-11-07 12:25:42 +0530

    feat: basic tabs setup in source form

---
+++ frontend/src/components/Settings/LeadSyncing/FailureLogs.vue
@@ -0,0 +1,12 @@
+<template>
+    Failure log list for source {{ props.source }} goes here...
+</template>
+
+<script setup>
+const props = defineProps({
+    source: {
+        type: String,
+        required: true
+    }
+})
+</script>
