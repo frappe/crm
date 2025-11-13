741c2501d82d380ebe9bce4023ae21fb490f38ec
shariquerik <sharique.rik@gmail.com>
2025-11-13 16:25:00 +0530

    refactor: add NotPermitted page and update routing logic for access control
    (cherry picked from commit 19d0a8a140011fa5e7da5aace01f7d445d545bdd)
    
    # Conflicts:
    #	frontend/src/App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <FrappeUIProvider>
+<<<<<<< HEAD
     <Layout v-if="session().isLoggedIn">
       <router-view :key="$route.fullPath"/>
+=======
+    <NotPermitted v-if="$route.name === 'Not Permitted'" />
+    <Layout class="isolate" v-else-if="session().isLoggedIn">
+      <router-view :key="$route.fullPath" />
+>>>>>>> 19d0a8a1 (refactor: add NotPermitted page and update routing logic for access control)
     </Layout>
     <Dialogs />
   </FrappeUIProvider>
 </template>
 
 <script setup>
+import NotPermitted from '@/pages/NotPermitted.vue'
 import { Dialogs } from '@/utils/dialogs'
 import { sessionStore as session } from '@/stores/session'
 import { setTheme } from '@/stores/theme'
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { usersStore } from '@/stores/users'
 import { sessionStore } from '@/stores/session'
 import { viewsStore } from '@/stores/views'
 
@@ -93,6 +94,11 @@ const routes = [
     name: 'Invalid Page',
     component: () => import('@/pages/InvalidPage.vue'),
   },
+  {
+    path: '/not-permitted',
+    name: 'Not Permitted',
+    component: () => import('@/pages/NotPermitted.vue'),
+  },
 ]
 
 const handleMobileView = (componentName) => {
@@ -106,8 +112,19 @@ let router = createRouter({
 
 router.beforeEach(async (to, from, next) => {
   const { isLoggedIn } = sessionStore()
+  const { users, isWebsiteUser } = usersStore()
 
-  if (to.name === 'Home' && isLoggedIn) {
+  if (isLoggedIn && !users.fetched) {
+    try {
+      await users.promise
+    } catch (error) {
+      console.error('Error loading users', error)
+    }
+  }
+
+  if (isLoggedIn && to.name !== 'Not Permitted' && isWebsiteUser()) {
+    next({ name: 'Not Permitted' })
+  } else if (to.name === 'Home' && isLoggedIn) {
     const { views, getDefaultView } = viewsStore()
     await views.promise
 
@@ -121,7 +138,11 @@ router.beforeEach(async (to, from, next) => {
     route_name = route_name || 'Leads'
 
     if (name && !is_standard) {
-      next({ name: route_name, params: { viewType: type }, query: { view: name } })
+      next({
+        name: route_name,
+        params: { viewType: type },
+        query: { view: name },
+      })
     } else {
       next({ name: route_name, params: { viewType: type } })
     }
