f416a2a566c07fde295e4415f1bb37a23ccfe4d1
flexGURU <mukunajohn329@gmail.com>
2026-01-12 17:07:47 +0300

    feat: enhance Yeastar settings with access token management and HTTP request handling

--- crm/fcrm/doctype/crm_yeastar_settings/crm_yeastar_settings.py
+++ crm/fcrm/doctype/crm_yeastar_settings/crm_yeastar_settings.py
@@ -5,67 +5,96 @@ import frappe
 from frappe.model.document import Document
 import requests
 import json
+from frappe.utils import getdate
+from datetime import datetime, timedelta
 
 
 class CRMYeastarSettings(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
+    from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
+    if TYPE_CHECKING:
+        from frappe.types import DF
 
-		access_token: DF.Data | None
-		enabled: DF.Check
-		password: DF.Data | None
-		refresh_token: DF.Data | None
-		request_url: DF.Data | None
-		username: DF.Data | None
+        access_token: DF.Data | None
+        access_token_expire_duration: DF.Int
+        access_token_expiry: DF.Datetime | None
+        enabled: DF.Check
+        password: DF.Data | None
+        refresh_token: DF.Data | None
+        refresh_token_expire_duration: DF.Int
+        request_url: DF.Data | None
+        username: DF.Data | None
+    # end: auto-generated types
+    pass
 
-	# end: auto-generated types
-	pass
+    def validate(self):
+        if self.enabled:
+            if self.generate_access_token():
+                frappe.msgprint("Access token generated successfully.")
 
-	def validate(self):
-		if self.enabled:
-			if self.generate_access_token():
-				frappe.msgprint("Access token generated successfully.")
+    def generate_access_token(self) -> bool:
 
-	def generate_access_token(self) -> bool:
+        request_url: str = self.request_url + "/get_token"
 
-		request_url: str = self.request_url + "/get_token"
+        payload = {
+            "username": self.username,
+            "password": self.password,
+        }
 
-		try:
-			payload = {
-				"username": self.username,
-				"password": self.password,
-			}
+        response = self.make_http_request(
+            endpoint=request_url,
+            request_type="GENERATE_TOKEN",
+            data=payload,
+        )
+        self.access_token = response.get("access_token")
+        self.refresh_token = response.get("refresh_token")
+        self.access_token_expiry = datetime.now() + timedelta(
+            seconds=response.get("access_token_expire_time")
+        )
+        self.refresh_token_expire_duration = response.get("refresh_token_expire_time")
+        self.access_token_expire_duration = response.get("access_token_expire_time")
 
-			response = requests.post(
-				request_url,
-				data=json.dumps(payload),
-				headers={"Content-Type": "application/json"},
-			)
+    def make_http_request(self, endpoint: str, request_type: str, data: dict) -> dict:
+        headers = {"Content-Type": "application/json"}
 
-			response.raise_for_status()
+        try:
+            response = requests.post(url=endpoint, json=data, headers=headers)
 
-			data = response.json()
+            response.raise_for_status()
+            response_data = response.json()
 
-			if data.get("errcode") != 0:
-				frappe.throw(
-					f"Error from Yeastar API: {data.get('errmsg', 'Unknown error')}"
-				)
+            if response_data.get("errcode") != 0:
+                frappe.throw(
+                    f"{response_data.get('errmsg', 'Unknown error')}. ERROR CODE: {response_data.get('errcode')}"
+                )
 
-			self.access_token = data.get("access_token")
-			self.refresh_token = data.get("refresh_token")
+            return response_data
 
-			return True
+        except Exception as e:
+            frappe.log_error(
+                title=f"Error while making request of type {request_type} to Yeastar API: {str(e)}",
+                message=frappe.get_traceback(),
+            )
+            frappe.throw("There was an error connecting to the Yeastar API.")
 
-		except Exception as e:
-			frappe.log_error(
-				frappe.get_traceback(),
-				f"Yeastar CRM Settings: Access Token Generation Failed, {str(e)}",
-			)
-			frappe.throw(
-				"Failed to generate access token for Yeastar CRM. Please check the logs for more details."
-			)
+    def refresh_access_token(self):
+        request_url: str = self.request_url + "/refresh_token"
+
+        payload = {
+            "refresh_token": self.refresh_token,
+        }
+
+        response = self.make_http_request(
+            endpoint=request_url,
+            request_type="REFRESH_TOKEN",
+            data=payload,
+        )
+
+        self.access_token = response.get("access_token")
+        self.refresh_token = response.get("refresh_token")
+        self.access_token_expiry = datetime.now() + timedelta(
+            seconds=response.get("access_token_expire_time")
+        )
