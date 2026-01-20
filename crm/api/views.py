f5d9c90ce0ea3fa0b6d21a7252849db173c5c19a
shariquerik <sharique.rik@gmail.com>
2026-01-20 16:15:59 +0530

    refactor: enhance default view generation and field ordering logic

--- crm/api/views.py
+++ crm/api/views.py
@@ -50,23 +50,83 @@ def get_current_view(doctype=None, view_name=None):
 	view = query.run(as_dict=True) or None
 
 	if not view:
-		return add_default_view(doctype)
+		return add_standard_view(doctype)
 
 	return view[0]
 
 
-def add_default_view(doctype):
-	columns = [
-		{"label": "Like", "type": "Data", "key": "_liked_by", "width": "50px"},
-		{"label": "Name", "type": "Data", "key": "name", "width": "16rem"},
-		{"label": "Last Updated On", "type": "Datetime", "key": "modified", "width": "8rem"},
-	]
+STANDARD_LIST_FIELDS = {
+	"name": {"label": "Name", "fieldtype": "Data", "fieldname": "name", "width": "16rem"},
+	"modified": {
+		"label": "Last Updated On",
+		"fieldtype": "Datetime",
+		"fieldname": "modified",
+		"width": "8rem",
+	},
+}
 
+
+def ordered_fieldnames(title_field, fields):
+	seen = set()
+	ordered = []
+
+	_fields = [f.get("fieldname") for f in fields if f.get("in_list_view")]
+
+	def add_field(fieldname):
+		if not fieldname or fieldname in seen:
+			return
+		seen.add(fieldname)
+		ordered.append(fieldname)
+
+	base_fields = [f for f in _fields if f not in {"name", "modified", title_field}]
+
+	if title_field:
+		add_field(title_field)
+		for fieldname in base_fields:
+			add_field(fieldname)
+		add_field("name")
+	else:
+		add_field("name")
+		for fieldname in base_fields:
+			add_field(fieldname)
+
+	add_field("modified")
+
+	return ordered
+
+
+def add_standard_view(doctype):
+	columns = [{"label": "Like", "type": "Data", "key": "_liked_by", "width": "50px"}]
 	rows = ["name"]
 
-	default_view = {
-		"name": "Default View (" + doctype + ")",
-		"label": "Default View",
+	meta = frappe.get_meta(doctype)
+	fields = meta.get("fields") or []
+	title_field = (meta.get("title_field") or "").strip() or None
+	ordered_fields = ordered_fieldnames(title_field, fields)
+	field_map = {f.get("fieldname"): f for f in fields if f.get("fieldname")}
+
+	if title_field:
+		STANDARD_LIST_FIELDS["name"]["width"] = "8rem"
+
+	for fieldname in ordered_fields:
+		field_def = STANDARD_LIST_FIELDS.get(fieldname) or field_map.get(fieldname)
+		if not field_def:
+			continue
+		columns.append(
+			{
+				"label": field_def.get("label"),
+				"type": field_def.get("fieldtype"),
+				"key": field_def.get("fieldname"),
+				"width": field_def.get("width") or "10rem",
+			}
+		)
+		field_key = field_def.get("fieldname")
+		if field_key and field_key not in rows:
+			rows.append(field_key)
+
+	standard_view = {
+		"name": f"Standard view ({doctype})",
+		"label": "List",
 		"type": "list",
 		"dt": doctype,
 		"columns": json.dumps(columns),
@@ -76,4 +136,4 @@ def add_default_view(doctype):
 		"is_standard": 1,
 	}
 
-	return default_view
+	return standard_view
