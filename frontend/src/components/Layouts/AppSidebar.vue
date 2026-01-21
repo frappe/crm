df2b9ff00716981fe8d0c0ae6dca5f1d9ed46dfc
shariquerik <sharique.rik@gmail.com>
2026-01-21 14:35:35 +0530

    refactor: Sidebar section and item UI
    (cherry picked from commit ed8f84859f582c389241046af631323089a754da)
    
    # Conflicts:
    #	frontend/src/components/Layouts/AppSidebar.vue

--- frontend/src/components/Layouts/AppSidebar.vue
+++ frontend/src/components/Layouts/AppSidebar.vue
@@ -7,14 +7,14 @@
       <UserDropdown :isCollapsed="isSidebarCollapsed" />
     </div>
     <div class="flex-1 overflow-y-auto">
-      <div class="mb-3 flex flex-col">
+      <div class="flex flex-col">
         <SidebarLink
           id="notifications-btn"
           :label="__('Notifications')"
           :icon="NotificationsIcon"
           :isCollapsed="isSidebarCollapsed"
           @click="() => toggleNotificationPanel()"
-          class="relative mx-2 my-0.5"
+          class="relative mx-2 my-[1.5px]"
         >
           <template #right>
             <Badge
@@ -30,11 +30,16 @@
         </SidebarLink>
       </div>
       <div v-for="view in allViews" :key="view.label">
+<<<<<<< HEAD
         <div
           v-if="!view.hideLabel && isSidebarCollapsed && view.views?.length"
           class="mx-2 my-2 h-1 border-b"
         />
         <Section
+=======
+        <div class="border-t mx-2 my-1.5" />
+        <CollapsibleSection
+>>>>>>> ed8f8485 (refactor: Sidebar section and item UI)
           :label="view.name"
           :hideLabel="view.hideLabel"
           :opened="view.opened"
@@ -42,11 +47,11 @@
           <template #header="{ opened, hide, toggle }">
             <div
               v-if="!hide"
-              class="flex cursor-pointer gap-1.5 px-1 text-base font-medium text-ink-gray-5 transition-all duration-300 ease-in-out"
+              class="flex items-center cursor-pointer gap-1.5 text-base text-ink-gray-5 transition-all duration-300 ease-in-out"
               :class="
                 isSidebarCollapsed
-                  ? 'ml-0 h-0 overflow-hidden opacity-0'
-                  : 'ml-2 mt-4 h-7 w-auto opacity-100'
+                  ? 'h-0 overflow-hidden opacity-0'
+                  : 'px-4 pt-[11px] pb-2.5 w-auto opacity-100'
               "
               @click="toggle()"
             >
@@ -65,7 +70,7 @@
               :label="__(link.label)"
               :to="link.to"
               :isCollapsed="isSidebarCollapsed"
-              class="mx-2 my-0.5"
+              class="mx-2 my-[1.5px]"
             />
           </nav>
         </Section>
@@ -187,7 +192,7 @@ import {
 import { capture } from '@/telemetry'
 import router from '@/router'
 import { useStorage } from '@vueuse/core'
-import { ref, reactive, computed, h, markRaw, onMounted } from 'vue'
+import { ref, reactive, computed, markRaw, onMounted } from 'vue'
 
 const { getPinnedViews, getPublicViews } = viewsStore()
 const { toggle: toggleNotificationPanel } = notificationsStore()
@@ -287,7 +292,7 @@ function parseView(views) {
 }
 
 function getIcon(routeName, icon) {
-  if (icon) return h('div', { class: 'size-auto' }, icon)
+  if (icon) return icon
 
   switch (routeName) {
     case 'Leads':
