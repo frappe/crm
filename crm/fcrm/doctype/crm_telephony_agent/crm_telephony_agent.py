992d9b32981032decfeac0fee1879c47fa872a2b
shariquerik <sharique.rik@gmail.com>
2026-03-12 14:26:15 +0530

    fix: update mobile number handling in CRMTelephonyAgent to ensure correct addition and removal in phone_nos

--- crm/fcrm/doctype/crm_telephony_agent/crm_telephony_agent.py
+++ crm/fcrm/doctype/crm_telephony_agent/crm_telephony_agent.py
@@ -19,26 +19,44 @@ class CRMTelephonyAgent(Document):
 
 		call_receiving_device: DF.Literal["Computer", "Phone"]
 		default_medium: DF.Literal["", "Twilio", "Exotel"]
-		exotel: DF.Check
 		exotel_number: DF.Data | None
 		mobile_no: DF.Data | None
 		phone_nos: DF.Table[CRMTelephonyPhone]
-		twilio: DF.Check
 		twilio_number: DF.Data | None
 		user: DF.Link
 		user_name: DF.Data | None
 	# end: auto-generated types
 
 	def validate(self):
+		self.update_phone_nos_based_on_mobile_no()
 		self.set_primary()
+
+	def update_phone_nos_based_on_mobile_no(self):
+		if not self.get_doc_before_save():
+			return
+
+		old_mobile_no = self.get_doc_before_save().mobile_no
+		new_mobile_no = self.mobile_no
+
+		if old_mobile_no != new_mobile_no:
+			# if new mobile no. is not in phone_nos table, then add it
+			if new_mobile_no and not any(phone.get("number") == new_mobile_no for phone in self.phone_nos):
+				self.append("phone_nos", {"number": new_mobile_no, "is_primary": 1})
+
+			# if old mobile no. is in phone_nos table, then remove it
+			for phone in self.phone_nos:
+				if phone.get("number") == old_mobile_no:
+					self.phone_nos.remove(phone)
+					break
 
 	def set_primary(self):
 		# Used to set primary mobile no.
-		if len(self.phone_nos) == 0:
-			self.mobile_no = ""
+		if len(self.phone_nos) == 0 and not self.mobile_no:
 			return
+		elif len(self.phone_nos) == 0 and self.mobile_no:
+			self.append("phone_nos", {"number": self.mobile_no, "is_primary": 1})
 
-		is_primary = [phone.number for phone in self.phone_nos if phone.get("is_primary")]
+		is_primary = [phone.get("number") for phone in self.phone_nos if phone.get("is_primary")]
 
 		if len(is_primary) > 1:
 			frappe.throw(
@@ -49,7 +67,7 @@ class CRMTelephonyAgent(Document):
 		for d in self.phone_nos:
 			if d.get("is_primary") == 1:
 				primary_number_exists = True
-				self.mobile_no = d.number
+				self.mobile_no = d.get("number")
 				break
 
 		if not primary_number_exists:
