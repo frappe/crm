b139375c1d8237b753cfb73d94fe373f03a11804
shariquerik <sharique.rik@gmail.com>
2026-03-27 18:44:58 +0530

    fix: implement restore demo data functionality in FCRM Settings
    (cherry picked from commit d1f68b444d16d196199f55d66a45896faa0f9a7d)

--- crm/fcrm/doctype/fcrm_settings/fcrm_settings.js
+++ crm/fcrm/doctype/fcrm_settings/fcrm_settings.js
@@ -2,41 +2,71 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("FCRM Settings", {
-  // refresh(frm) {
+	refresh(frm) {
+		if (
+			(frappe.user.has_role("Sales Manager") ||
+				frappe.user.has_role("System Manager")) &&
+			frappe.boot.user.defaults.crm_demo_data_created !== "1"
+		) {
+			frm.set_df_property("restore_demo_data", "hidden", false);
+		} else {
+			frm.set_df_property("restore_demo_data", "hidden", true);
+		}
+	},
+	restore_defaults: function (frm) {
+		let message = __(
+			"This will restore (if not exist) all the default statuses, custom fields and layouts. Delete & Restore will delete default layouts and then restore them."
+		);
+		let d = new frappe.ui.Dialog({
+			title: __("Restore Defaults"),
+			primary_action_label: __("Restore"),
+			primary_action: () => {
+				frm.call("restore_defaults", { force: false }, () => {
+					frappe.show_alert({
+						message: __(
+							"Default statuses, custom fields and layouts restored successfully."
+						),
+						indicator: "green"
+					});
+				});
+				d.hide();
+			},
+			secondary_action_label: __("Delete & Restore"),
+			secondary_action: () => {
+				frm.call("restore_defaults", { force: true }, () => {
+					frappe.show_alert({
+						message: __(
+							"Default statuses, custom fields and layouts restored successfully."
+						),
+						indicator: "green"
+					});
+				});
+				d.hide();
+			}
+		});
+		d.show();
+		d.set_message(message);
+	},
 
-  // },
-  restore_defaults: function (frm) {
-    let message = __(
-      "This will restore (if not exist) all the default statuses, custom fields and layouts. Delete & Restore will delete default layouts and then restore them.",
-    );
-    let d = new frappe.ui.Dialog({
-      title: __("Restore Defaults"),
-      primary_action_label: __("Restore"),
-      primary_action: () => {
-        frm.call("restore_defaults", { force: false }, () => {
-          frappe.show_alert({
-            message: __(
-              "Default statuses, custom fields and layouts restored successfully.",
-            ),
-            indicator: "green",
-          });
-        });
-        d.hide();
-      },
-      secondary_action_label: __("Delete & Restore"),
-      secondary_action: () => {
-        frm.call("restore_defaults", { force: true }, () => {
-          frappe.show_alert({
-            message: __(
-              "Default statuses, custom fields and layouts restored successfully.",
-            ),
-            indicator: "green",
-          });
-        });
-        d.hide();
-      },
-    });
-    d.show();
-    d.set_message(message);
-  },
+	restore_demo_data: function (frm) {
+		let d = new frappe.ui.Dialog({
+			title: __("Restore Demo Data"),
+			primary_action_label: __("Restore"),
+			primary_action: () => {
+				frm.call("restore_demo_data", { force: false }, () => {
+					frappe.show_alert({
+						message: __("Demo data restored successfully."),
+						indicator: "green"
+					});
+				});
+				d.hide();
+			}
+		});
+		d.show();
+		d.set_message(
+			__(
+				"This will restore the demo data. Are you sure you want to continue?"
+			)
+		);
+	}
 });
