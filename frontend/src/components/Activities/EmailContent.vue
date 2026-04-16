2cd2fcf7f012922c0e417b777492b5c6e4d6722e
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-04-16 12:24:52 +0530

    fix: use `meta.glob` to import index.css

--- frontend/src/components/Activities/EmailContent.vue
+++ frontend/src/components/Activities/EmailContent.vue
@@ -13,7 +13,7 @@ const props = defineProps({
   content: { type: String, required: true },
 })
 
-const files = import.meta.globEager('/src/index.css', { query: '?inline' })
+const files = import.meta.glob('/src/index.css', { eager: true, query: '?inline' });
 const css = files['/src/index.css'].default
 
 const iframeRef = ref(null)
