43c282c8c3976171031105a4074746a306b17344
Balamurugan <139839683+BalamuruganRamachandran-Techfinite@users.noreply.github.com>
2026-05-14 14:04:18 +0530

    resolved whatsapp api issue

--- frontend/src/composables/settings.js
+++ frontend/src/composables/settings.js
@@ -4,7 +4,7 @@ import { computed, ref } from 'vue'
 export const whatsappEnabled = ref(false)
 export const isWhatsappInstalled = ref(false)
 createResource({
-  url: 'crm.api.whatsapp.is_whatsapp_enabled',
+  url: '/api/method/crm.api.whatsapp.is_whatsapp_enabled',
   cache: 'Is Whatsapp Enabled',
   auto: true,
   onSuccess: (data) => {
@@ -12,7 +12,7 @@ createResource({
   },
 })
 createResource({
-  url: 'crm.api.whatsapp.is_whatsapp_installed',
+  url: '/api/method/crm.api.whatsapp.is_whatsapp_installed',
   cache: 'Is Whatsapp Installed',
   auto: true,
   onSuccess: (data) => {
