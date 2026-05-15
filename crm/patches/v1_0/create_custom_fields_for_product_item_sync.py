00751071bf9200eabb31bb16dc670433d9b512e4
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-05-15 11:06:03 +0530

    feat: sync hooks
    (cherry picked from commit c268948f188e996c396145cd176a30d1e33d0530)

---
+++ crm/patches/v1_0/create_custom_fields_for_product_item_sync.py
@@ -0,0 +1,31 @@
+import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+
+
+def execute():
+	if not frappe.db.get_single_value("ERPNext CRM Settings", "enabled"):
+		return
+
+	create_custom_fields(
+		{
+			"CRM Product": [
+				{
+					"fieldname": "erpnext_item_code",
+					"fieldtype": "Data",
+					"label": "Item Code in ERPNext",
+					"read_only": 1,
+					"insert_after": "product_code",
+				}
+			]
+		},
+		ignore_validate=True,
+	)
+
+	if frappe.db.get_single_value("ERPNext CRM Settings", "is_erpnext_in_different_site"):
+		return
+
+	try:
+		from erpnext.crm.frappe_crm_api import create_custom_fields_for_frappe_crm
+	except ImportError:
+		return
+	create_custom_fields_for_frappe_crm()
