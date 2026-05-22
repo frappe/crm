e700aed38e3578de05852ede378e3857f981450d
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-05-22 13:59:55 +0530

    fix: show banner when atleast one lead is created

--- crm/www/crm.py
+++ crm/www/crm.py
@@ -45,9 +45,7 @@ def get_boot():
 			"is_demo_site": frappe.conf.get("is_demo_site"),
 			"demo_data_created": frappe.db.get_default("crm_demo_data_created") == "1",
 			"is_fc_site": is_fc_site(),
-			"show_sales_hierarchy_banner": cint(
-				frappe.db.get_single_value("FCRM Settings", "show_sales_hierarchy_banner")
-			),
+			"show_sales_hierarchy_banner": frappe.db.count("CRM Lead") > 0,
 			"translated_doctypes": get_translated_doctypes(),
 			"translated_messages": get_messages_for_boot(),
 			"timezone": {
