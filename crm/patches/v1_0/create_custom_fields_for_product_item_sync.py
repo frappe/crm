643a3377acb1f4ab6d3b31b70694fe525fba078f
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-05-26 23:06:18 +0530

    fix: create custom fields for Product Item sync

--- crm/patches/v1_0/create_custom_fields_for_product_item_sync.py
+++ crm/patches/v1_0/create_custom_fields_for_product_item_sync.py
@@ -6,20 +6,28 @@ def execute():
 	if not frappe.db.get_single_value("ERPNext CRM Settings", "enabled"):
 		return
 
-	create_custom_fields(
+	custom_fields = {
+		"CRM Product": [
+			{
+				"fieldname": "erpnext_item_code",
+				"fieldtype": "Data",
+				"label": "Item Code in ERPNext",
+				"read_only": 1,
+				"insert_after": "product_code",
+			}
+		]
+	}
+	custom_fields["Item"] = [
 		{
-			"CRM Product": [
-				{
-					"fieldname": "erpnext_item_code",
-					"fieldtype": "Data",
-					"label": "Item Code in ERPNext",
-					"read_only": 1,
-					"insert_after": "product_code",
-				}
-			]
-		},
-		ignore_validate=True,
-	)
+			"fieldname": "crm_product_code",
+			"fieldtype": "Data",
+			"label": "CRM Product",
+			"read_only": 1,
+			"no_copy": 1,
+			"insert_after": "item_code",
+		}
+		]
+	create_custom_fields(custom_fields, ignore_validate=True)
 
 	if frappe.db.get_single_value("ERPNext CRM Settings", "is_erpnext_in_different_site"):
 		return
