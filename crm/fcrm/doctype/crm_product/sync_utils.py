b509729aa41e0c4108ad975c12d31a54cafc36a2
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-05-27 09:01:29 +0530

    feat: sync utils
    (cherry picked from commit c43af6c7de80e7c3c190b736cb6478e8a04ef49b)
    (cherry picked from commit b4f28c48425388ef747d1e48aebee7bcb176749c)

---
+++ crm/fcrm/doctype/crm_product/sync_utils.py
@@ -0,0 +1,9 @@
+import frappe
+
+
+def payload_differs(payload: dict, target: dict) -> bool:
+	"""True if any field in payload has a meaningfully different value in target."""
+	for key, new_value in payload.items():
+		if (new_value or None) != (target.get(key) or None):
+			return True
+	return False
