c9c1d753b678a4ded5b04d0cc9846d493cbcf3b1
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-06-03 16:53:23 +0530

    feat: add client script to create customer when sales order is created
    (cherry picked from commit 58f34b46162cc3ba4a01e5f0ffb190e0dc149ffa)

---
+++ crm/public/js/erpnext_sales_order_customer.js
@@ -0,0 +1,17 @@
+frappe.ui.form.on('Sales Order', {
+	refresh(frm) {
+		// When a Sales Order is created from a CRM Deal quotation that has no
+		// customer yet, create/fetch the customer and fill the mandatory field.
+		if (frm.doc.docstatus !== 0 || frm.doc.customer || frm.__crm_customer_checked) return
+		const item = (frm.doc.items || []).find((i) => i.prevdoc_docname)
+		if (!item) return
+		frm.__crm_customer_checked = true
+		frappe.call({
+			method: 'crm.fcrm.doctype.erpnext_crm_settings.erpnext_crm_settings.ensure_customer_for_quotation',
+			args: { quotation: item.prevdoc_docname },
+			callback: (r) => {
+				if (r.message) frm.set_value('customer', r.message)
+			},
+		})
+	},
+})
