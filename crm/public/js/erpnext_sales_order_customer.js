a94b88b702b64530578b31058744884ffb02486b
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-06-05 13:58:29 +0530

    chore: rename method
    (cherry picked from commit 7d32ac4d0b57f38e12b085bad0974ff52d1d888e)
    (cherry picked from commit d0c09cbd12f0c2fccb2da0692518a2bec10862dc)

--- crm/public/js/erpnext_sales_order_customer.js
+++ crm/public/js/erpnext_sales_order_customer.js
@@ -7,7 +7,7 @@ frappe.ui.form.on('Sales Order', {
 		if (!item) return
 		frm.__crm_customer_checked = true
 		frappe.call({
-			method: 'crm.fcrm.doctype.erpnext_crm_settings.erpnext_crm_settings.ensure_customer_for_quotation',
+			method: 'crm.fcrm.doctype.erpnext_crm_settings.erpnext_crm_settings.check_customer_for_quotation',
 			args: { quotation: item.prevdoc_docname },
 			callback: (r) => {
 				if (r.message) frm.set_value('customer', r.message)
