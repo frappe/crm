217c9b720b0432ad96ea6d76cb791471bac0bd85
Adnan Quazi <quaziadnan12352@gmail.com>
2026-06-06 21:11:58 +0530

    fix: use _add instead of add for agent assignment
    add() doesn't accept ignore_permissions, causing TypeError during after_insert. Switch to _add to correctly bypass permission checks in server-side assignment context.

--- crm/fcrm/doctype/crm_lead/crm_lead.py
+++ crm/fcrm/doctype/crm_lead/crm_lead.py
@@ -5,7 +5,7 @@ import json
 
 import frappe
 from frappe import _
-from frappe.desk.form.assign_to import add as assign
+from frappe.desk.form.assign_to import _add as assign
 from frappe.model.document import Document
 from frappe.utils import has_gravatar, validate_email_address
 
