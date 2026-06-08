394a6b2bac9e5f967ed3f1660cc7bb150d0c2fb1
Ahmad Salamouny <Ahmad.salamouny@our-edu.net>
2026-06-08 09:33:06 +0000

    fix: update fields retrieval in update_deals_email_mobile_no function for dynamic parenttype to work for leads and deals

--- crm/api/contact.py
+++ crm/api/contact.py
@@ -10,14 +10,14 @@ def update_deals_email_mobile_no(doc):
 	linked_deals = frappe.get_all(
 		"CRM Contacts",
 		filters={"contact": doc.name, "is_primary": 1},
-		fields=["parent"],
+		fields=["parenttype", "parent"],
 	)
 
 	for linked_deal in linked_deals:
-		deal = frappe.db.get_values("CRM Deal", linked_deal.parent, ["email", "mobile_no"], as_dict=True)[0]
+		deal = frappe.db.get_values(linked_deal.parenttype, linked_deal.parent, ["email", "mobile_no"], as_dict=True)[0]
 		if deal.email != doc.email_id or deal.mobile_no != doc.mobile_no:
 			frappe.db.set_value(
-				"CRM Deal",
+				linked_deal.parenttype,
 				linked_deal.parent,
 				{
 					"email": doc.email_id,
