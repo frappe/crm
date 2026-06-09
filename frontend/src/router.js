2339011232c1e493f432cac4f33351dae8191fb9
Adnan Quazi <quaziadnan12352@gmail.com>
2026-06-09 14:40:20 +0530

    fix: resolve linting warnings
    (cherry picked from commit f71683cc8e3b14b4bb5597d3a358b256109fc454)

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -160,6 +160,7 @@ router.beforeEach(async (to, from, next) => {
     try {
       await users.promise
     } catch (error) {
+      console.error('Error loading users', error)
       return next(false)
     }
   }
