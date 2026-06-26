2601872b88db186196839845ad346fc1712097c2
mahsem <137205921+mahsem@users.noreply.github.com>
2026-06-26 06:20:23 +0200

    fix: rename_Accounts_to_Email_Accounts

--- frontend/src/components/Settings/Settings.vue
+++ frontend/src/components/Settings/Settings.vue
@@ -174,7 +174,7 @@ const tabs = computed(() => {
       label: __('Email'),
       items: [
         {
-          label: __('Accounts'),
+          label: __('Email Accounts'),
           icon: Email2Icon,
           component: markRaw(EmailConfig),
           condition: () => isManager(),
