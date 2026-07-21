f6fdaed63cd1aed2c344169271a4745a18f3b504
georgekimanjoroge <kimaniixv@gmail.com>
2026-07-21 14:05:39 +0300

    chore: restore main.js and SidePanelLayout.vue to develop state

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,7 +7,6 @@ import { initSocket } from './socket'
 import router from './router'
 import translationPlugin from './translation'
 import App from './App.vue'
-import { preloadCurrencySymbolPlacement } from './utils/numberFormat'
 
 import {
   FrappeUI,
@@ -38,7 +37,9 @@ let globalComponents = {
   FeatherIcon,
 }
 
+// create a pinia instance
 let pinia = createPinia()
+
 let app = createApp(App)
 
 setConfig('resourceFetcher', frappeRequest)
@@ -50,27 +51,25 @@ for (let key in globalComponents) {
   app.component(key, globalComponents[key])
 }
 app.use(telemetryPlugin, { app_name: 'crm' })
+
 app.config.globalProperties.$dialog = createDialog
 
 let socket
-async function mountApp() {
-  await preloadCurrencySymbolPlacement()
-  socket = initSocket()
-  app.config.globalProperties.$socket = socket
-  app.mount('#app')
-}
-
 if (import.meta.env.DEV) {
   frappeRequest({ url: '/api/method/crm.www.crm.get_context_for_dev' }).then(
-    async (values) => {
+    (values) => {
       for (let key in values) {
         window[key] = values[key]
       }
-      await mountApp()
+      socket = initSocket()
+      app.config.globalProperties.$socket = socket
+      app.mount('#app')
     },
   )
 } else {
-  mountApp()
+  socket = initSocket()
+  app.config.globalProperties.$socket = socket
+  app.mount('#app')
 }
 
 if (import.meta.env.DEV) {
