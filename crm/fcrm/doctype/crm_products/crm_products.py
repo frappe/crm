2ab51affa149f2a574fa9b7ac893040517209b8a
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-07-29 21:42:56 +0530

    chore: regenerate CRM Products types

--- crm/fcrm/doctype/crm_products/crm_products.py
+++ crm/fcrm/doctype/crm_products/crm_products.py
@@ -15,7 +15,6 @@ class CRMProducts(Document):
 		from frappe.types import DF
 
 		amount: DF.Currency
-		autocomplete: DF.Autocomplete | None
 		discount_amount: DF.Currency
 		discount_percentage: DF.Percent
 		net_amount: DF.Currency
