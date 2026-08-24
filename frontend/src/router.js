7260c150784f1d4a1a931050e42dc81b232d37f9
AdnanQuazi <quaziadnan12352@gmail.com>
2026-08-24 17:31:03 +0000

    fix: prevent blank screen on fetch failure
    (cherry picked from commit e6647764afa3abe90d2fba7337c77fcad0662471)

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -161,7 +161,9 @@ router.beforeEach(async (to, from, next) => {
       await users.promise
     } catch (error) {
       console.error('Error loading users', error)
-      return next(false)
+      if (error?.exc_type !== 'PermissionError') {
+        return next()
+      }
     }
   }
 
