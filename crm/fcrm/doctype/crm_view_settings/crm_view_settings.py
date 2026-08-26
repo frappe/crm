f3823ac5b171dbb9c137b218b93858b7b0452c7c
Asim Delvi <asimdelvi30@gmail>
2026-08-26 23:03:35 +0530

    fix: remove logger
    (cherry picked from commit 151d776e02bbc9115c3d378b50857f8c285d1486)

--- crm/fcrm/doctype/crm_view_settings/crm_view_settings.py
+++ crm/fcrm/doctype/crm_view_settings/crm_view_settings.py
@@ -3,7 +3,7 @@
 import json
 
 import frappe
-from frappe import _, log
+from frappe import _
 from frappe.model.document import Document, get_controller
 from frappe.utils import parse_json
 
@@ -205,7 +205,6 @@ def set_as_default(name: str | int | None = None, type: str | None = None, docty
 		doc = create_or_update_standard_view({"type": type, "doctype": doctype, "is_default": 1})
 		name = doc.name
 
-	frappe.log(f"Setting view {name}, type {type}, doctype {doctype} as default for user {frappe.session.user}")
 	# remove default from other views of same user and same doctype
 	frappe.db.set_value(
 		"CRM View Settings",
