d0dc954e7ffdb56edfe4c4c24f0c8d6e390b2876
Shindhu R <shindhuramaswamy01@gmail.com>
2026-08-28 15:21:26 +0530

    fix: field layout change

--- frontend/src/components/RichTextField.vue
+++ frontend/src/components/RichTextField.vue
@@ -1,20 +1,22 @@
 <template>
-  <Editor
-    v-model="content"
-    :extensions="extensions"
-    :placeholder="placeholder"
-    :editable="editable"
-    :upload-function="(file) => uploadFile(file)"
-    @change="(val) => emit('change', val)"
-  >
-    <EditorFixedMenu
-      v-if="fixedMenu"
-      :items="fullToolbar"
-      class="w-full overflow-x-auto rounded-t-lg border border-outline-gray-2 p-1"
-    />
-    <EditorBubbleMenu v-if="bubbleMenu" :items="bubbleToolbar" />
-    <EditorContent :class="editorClass" />
-  </Editor>
+  <div>
+    <Editor
+      v-model="content"
+      :extensions="extensions"
+      :placeholder="placeholder"
+      :editable="editable"
+      :upload-function="(file) => uploadFile(file)"
+      @change="(val) => emit('change', val)"
+    >
+      <EditorFixedMenu
+        v-if="fixedMenu"
+        :items="fullToolbar"
+        class="w-full overflow-x-auto rounded-t-lg border border-b-0 border-outline-gray-2 p-1"
+      />
+      <EditorBubbleMenu v-if="bubbleMenu" :items="bubbleToolbar" />
+      <EditorContent :class="editorClass" />
+    </Editor>
+  </div>
 </template>
 
 <script setup>
