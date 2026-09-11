c40e6ec2c422dd01905ab36fc7f1423387ca3f04
Pratham <prathamsharma173@gmail.com>
2026-09-11 15:04:25 +0530

    chore: remove calendar feature from frontend
    (cherry picked from commit 30a1f04fa1748c9ac3611266de370c4eeee71832)
    
    # Conflicts:
    #	frontend/src/components/Activities/AllModals.vue
    #	frontend/src/components/Notifications.vue

--- frontend/src/components/Notifications.vue
+++ frontend/src/components/Notifications.vue
@@ -72,6 +72,7 @@
           </div>
         </RouterLink>
       </div>
+<<<<<<< HEAD
       <EmptyState
         v-else
         title="No New Notifications"
@@ -79,6 +80,9 @@
         :icon="NotificationsIcon"
         width="lg"
       />
+=======
+      <div v-else class="flex h-full"></div>
+>>>>>>> 30a1f04 (chore: remove calendar feature from frontend)
     </div>
   </div>
 </template>
@@ -103,6 +107,15 @@ const { $socket } = globalStore()
 const { mark_as_read, toggle, mark_doc_as_read } = notificationsStore()
 const { capture } = useTelemetry()
 
+<<<<<<< HEAD
+=======
+const activeTab = ref('all')
+const tabs = [
+  { label: __('All'), value: 'all' },
+  // { label: __('Mentions'), value: 'mentions' },
+]
+
+>>>>>>> 30a1f04 (chore: remove calendar feature from frontend)
 const target = ref(null)
 onClickOutside(
   target,
@@ -129,9 +142,13 @@ onBeforeUnmount(() => {
 })
 
 onMounted(() => {
+<<<<<<< HEAD
   $socket.on('crm_notification', () => {
     notifications.reload()
   })
+=======
+  $socket.on('crm_notification', () => notifications.reload())
+>>>>>>> 30a1f04 (chore: remove calendar feature from frontend)
 })
 
 function getRoute(notification) {
